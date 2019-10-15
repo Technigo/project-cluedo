b289364e690ce6dc908d1e5cb6aac5d58752d833
Joanna Pernu <joanna.pernu@gmail.com>
2019-10-15 13:05:54 +0200

    Late push, updated version of java, code there but doent seem to work.

--- code/clue.js
+++ code/clue.js
@@ -1,4 +1,4 @@
-// CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
+// SUSPECTS//////////////////////////
 
 const mrGreen = {
   firstName: "Jacob",
@@ -10,29 +10,139 @@ const mrGreen = {
   occupation: "Entrepreneur"
 }
 
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
+const proffesorPlum = {
+    firstName: "Victor",
+    lastName: "Plum",
+    color: "orange",
+    description: "Selfmade video-game billionaire",
+    age: 36,
+    image: "assets/plum.png",
+    occupation: "Video-game designer"
+  }
+
+const missScarlet = {
+    firstName: "Cassandra",
+    lastName: "Scarlet",
+    color: "red",
+    description: "Center of every social event",
+    age: 25,
+    image: "assets/scarlet.png",
+    occupation: "Movie star"
+  }
+
+const mrsPeacock = {
+  firstName: "Elizabeth",
+    lastName: "Peacock",
+    color: "blue",
+    description: "Elegant aristocrat with Queen-like resemblance",
+    age: 32,
+    image: "assets/peacock.png",
+    occupation: "Socialite"
+}
+
+const colonelMustard {
+firstName: "Michael",
+    lastName: "Mustard",
+    color: "Yellow",
+    description: "Dignified and dangerous",
+    age: 60,
+    image: "assets/mustard.png",
+    occupation: "Military"
+} 
+ 
+const mrsWhite {
+  firstName: "Blance",
+  lastName: "White",
+  color: "White",
+  description: "Cheerful",
+  age: 55,
+  image: "assets/white.png",
+  occupation: "Household cook"
+}
+// WEAPONS //////////////////
 
 const rope = {
   name: "rope",
-  weight: 10
+  identifier: "from the stables" 
 }
 
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+const knife ={
+  name: "knife",
+  identifier:"chefs knife",
+}
+
+const candlestick = {
+  name: "candlestick",
+  identifier: 20
+}
+
+const dumbbell ={
+  name: "dumbbell",
+  identifier: "heavy"
+}
+
+const poison = {
+  name: "poison",
+  identifier: "tasteless",
+}
+const axe ={
+  name: "axe",
+  identifier: "antics"
+}
+
+const bat = {
+  name: "bat",
+  identifier: "made of aluminium",
+}
+const trophy = {
+  name: "trophy",
+  identifier: "Mrs Peacocks trophy for volunteering"
+}
+const pistol = {
+  name: "pistol",
+  identifier: "magnum",
+}
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
 
 const suspects = [
   mrGreen,
   mrsWhite,
-  profPlum,
+  proffesorPlum,
   missScarlet,
   mrsPeacock,
-  mrMustard
+  colonelMustard
 ]
 
-const weapons = []
+const weapons = [
+rope,
+knife,
+candlestick,
+dumbbell,
+poison,
+axe,
+bat,
+trophy,
+pistol,
+]
 
-const rooms = []
+const rooms = [
+"Dinning Room",
+"Conservatory",
+"Kitchen",
+"Study",
+"Library",
+"Billiard Room",
+"Lounge",
+"Ballroom",
+"Hall",
+"Spa",
+"Living Room",
+"Observatory",
+"Theater",
+"Guest House",
+"Patio"
+]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
@@ -59,6 +169,13 @@ const pickKiller = () => {
 }
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+const pickWeapon = () => {
+  mystery.weapon = randomSelector(weapons)
+}
 
+const pickRoom = () => {
+  mystery.room = randomSelector(rooms)
+}
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+console.log("The murder was comitted by", mystery.killer.lastName, "in the", mysteryRoom, "with the", mystery.weapon"." )
