a68efe25adb27504aef35b269b275fcefd0e54f6
tehvicke <vikda574@student.liu.se>
2019-10-21 16:22:00 +0200

    added check to see if all cards are clicked

--- code/clue.js
+++ code/clue.js
@@ -20,6 +20,16 @@ const rooms = [
   "Hallway"
 ]
 
+ // Boolean to tell if a card has been clicked
+const cardClicked = {
+  killer: false,
+  weapon: false,
+  room: false,
+  allAreClicked: function() {
+    return this.killer && this.weapon && this.room
+  }
+}
+
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
   return array[Math.floor(Math.random() * array.length)]
@@ -31,6 +41,7 @@ const mystery = {
   weapon,
   room
 }
+// console.log(mystery.killer.type)
 // With a killer, a weapon and a room.
 // The values will be set later.
 
@@ -53,21 +64,20 @@ const pickKiller = () => {
   theKillerOccupation.innerHTML = "Occupation: " + mystery.killer.occupation
   theKillerImage.src = mystery.killer.image
   theKillerDescription.innerHTML = mystery.killer.description
+  cardClicked.killer = true
 }
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 const pickWeapon = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.weapon = randomSelector(weapons)
-
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
   const theWeapon = document.getElementById("weapon")
   const theWeaponName = document.getElementById("weaponName")
   const theWeaponWeight = document.getElementById("weaponWeight")
   theWeapon.style.background = "#333"
-  // theKillerImage.src = mystery.killer.image
   theWeaponName.innerHTML = "Weapon: " + mystery.weapon.name
   theWeaponWeight.innerHTML = "Weight: " + mystery.weapon.weight  
+  cardClicked.weapon = true
 }
 const pickRoom = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
@@ -79,10 +89,17 @@ const pickRoom = () => {
   theRoom.style.background = "#991"
   // theKillerImage.src = mystery.killer.image
   theRoomName.innerHTML = mystery.room
+  cardClicked.room = true
 }
 
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 const revealMystery = () => {
+  if (!cardClicked.allAreClicked()) {
+    alert('No mystery is to be solved')
+    return
+  }
+  
   alert(`The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room.toLowerCase()} with a ${mystery.weapon.name.toLowerCase()}.`)
+  location.reload()
 }
