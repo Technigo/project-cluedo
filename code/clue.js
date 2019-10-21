f7586e93fabaef1a08c67f5e43a2aff6d2b24e6a
Linda <isell.linda@gmail.com>
2019-10-21 17:29:37 +0200

    step 1 done

--- code/clue.js
+++ code/clue.js
@@ -1,33 +1,131 @@
 // CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
 
 const mrGreen = {
-  firstName: "Jacob",
-  lastName: "Green",
-  color: "green",
-  description: "He has a lot of connections",
-  age: 45,
-  image: "assets/green.png",
-  occupation: "Entrepreneur"
+    firstName: "Jacob",
+    lastName: "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "assets/green.png",
+    occupation: "Entrepreneur"
 }
+
+const professorPlum = {
+    firstName: "Victor",
+    lastName: "Plum",
+    color: "purple",
+    description: "A quick-witted middle-aged man",
+    age: 35,
+    image: "assets/plum.png",
+    occupation: "Professor"
+}
+const missScarlet = {
+    firstName: "Cassandra",
+    lastName: "Scarlet",
+    color: "red",
+    description: "A stock femme fatale",
+    age: 25,
+    image: "assets/scarlet.png",
+    occupation: "Singer"
+}
+const mrsPeacock = {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    color: "brown",
+    description: "An elderly woman who nearly always maintains her dignity",
+    age: 60,
+    image: "assets/peacock.png",
+    occupation: "Professor"
+}
+const mrsWhite = {
+    firstName: "Mrs",
+    lastName: "White",
+    color: "white",
+    description: "A frazzled servant",
+    age: 40,
+    image: "assets/white.png",
+    occupation: "Servant"
+}
+const mrMustard = {
+    firstName: "Colonel",
+    lastName: "Mustard",
+    color: "yellow",
+    description: "A dignified, dapper and dangerous military man",
+    age: 55,
+    image: "assets/mustard.png",
+    occupation: "General"
+}
+
+
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
 const rope = {
-  name: "rope",
-  weight: 10
+    name: "rope",
+    weight: 10
+}
+const knife = {
+    name: "knife",
+    weight: 2
+}
+const candlestick = {
+    name: "Candlestick",
+    weight: 3
+}
+const dumbbell = {
+    name: "Dumbbell",
+    weight: 15
+}
+const poison = {
+    name: "Poison",
+    weight: 1
+}
+const axe = {
+    name: "Axe",
+    weight: 8
+}
+const bat = {
+    name: "Bat",
+    weight: 2
+}
+const trophy = {
+    name: "Trophy",
+    weight: 12
+}
+const pistol = {
+    name: "Pistol",
+    weight: 6
 }
 
+
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+
+const dinningRoom
+const conservatory
+const kitchen
+const study
+const library
+const billiardRoom
+const lounge
+const ballroom
+const hall
+const spa
+const livingRoom
+const observatory
+const theater
+const guestHouse
+const patio
+
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
 
 const suspects = [
-  mrGreen,
-  mrsWhite,
-  profPlum,
-  missScarlet,
-  mrsPeacock,
-  mrMustard
+    mrGreen,
+    mrsWhite,
+    profPlum,
+    missScarlet,
+    mrsPeacock,
+    mrMustard
 ]
 
 const weapons = []
@@ -36,7 +134,7 @@ const rooms = []
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
-  return array[Math.floor(Math.random() * array.length)]
+    return array[Math.floor(Math.random() * array.length)]
 }
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
@@ -46,16 +144,16 @@ const randomSelector = array => {
 // FINISH THIS FUNCTION TO SHOW ALL INFORMATION ABOUT THE KILLER.
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
-  // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
+    // This will randomly select a killer from the suspects. And add that to the mystery object.
+    mystery.killer = randomSelector(suspects)
 
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
-  const theKiller = document.getElementById("killer")
-  const theKillerName = document.getElementById("killerName")
+    // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
+    const theKiller = document.getElementById("killer")
+    const theKillerName = document.getElementById("killerName")
 
-  theKiller.style.background = mystery.killer.color
-  theKillerName.innerHTML =
-    mystery.killer.firstName + " " + mystery.killer.lastName
+    theKiller.style.background = mystery.killer.color
+    theKillerName.innerHTML =
+        mystery.killer.firstName + " " + mystery.killer.lastName
 }
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
