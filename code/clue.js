836239b3633d6dbc738ba88a77cda3b10d59e68e
Petra B <petrabrinck@icloud.com>
2019-10-22 15:18:20 +0200

    step 1 done

--- code/clue.js
+++ code/clue.js
@@ -1,23 +1,126 @@
 // CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
 
-const mrGreen = {
+const mrFGreen = {
   firstName: "Jacob",
   lastName: "Green",
   color: "green",
   description: "He has a lot of connections",
-  age: 45,
+  age: 39,
   image: "assets/green.png",
   occupation: "Entrepreneur"
 }
+/// console.log(mrgreen.firstName) //////
+
+const profPlum = {
+  firstName: "Victor",
+  lastName: "Plum",
+  color: "purple",
+  description: "He is a small fat man",
+  age: 56,
+  image: "assets/plum.png",
+  occupation: "Lawyer"
+}
+
+const missScarlet = {
+  firstName: "Cassandra",
+  lastName: "Scarlet",
+  color: "orange",
+  description: "She is a tall intelligent woman",
+  age: 33,
+  image: "assets/scarlet.png",
+  occupation: "Tech entrepreneur"
+}
+
+const mrsPeacock = {
+  firstName: "Eleanor",
+  lastName: "Peacock",
+  color: "purple",
+  description: "She devious middle aged woman",
+  age: 56,
+  image: "assets/peacock.png",
+  occupation: "Media woman"
+}
+
+const colonelMustard = {
+  firstName: "Jack",
+  lastName: "Mustard",
+  color: "yellow",
+  description: "He is a handsome former Navy Seal",
+  age: 61,
+  image: "assets/mustard.png",
+  occupation: "Retired Navy SEAL"
+}
+
+const mrsWhite = {
+  firstName: "Alice",
+  lastName: "White",
+  color: "white",
+  description: "She is a nurse",
+  age: 24,
+  image: "assets/white.png",
+  occupation: "Nurse at County General Hospital Chicago "
+}
+
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: "rope",
-  weight: 10
+  weight: 10,
+  color: "brown"
 }
 
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+const knife = {
+  name: "knife",
+  weight: 25,
+  color: "brown"
+}
+
+const candlestick = {
+  name: "candlestick",
+  weight: 20,
+  color: "silver"
+}
+
+const dumbbell = {
+  name: "dumbbell",
+  weight: 40,
+  color: "pink"
+}
+
+const poison = {
+  name: "poison",
+  weight: 2,
+  color: "black"
+}
+
+const axe = {
+  name: "axe",
+  weight: 35,
+  color: "lightbrown"
+}
+
+const bat = {
+  name: "bat",
+  weight: 35,
+  color: "red"
+}
+
+const trophy = {
+  name: "trophy",
+  weight: 60,
+  color: "gold"
+}
+
+const pistol = {
+  name: "pistol",
+  weight: 30,
+  color: "black"
+}
+
+
+// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE. //KONTROLLERA KOMMATECKEN PÅ RUMMEN?
+
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
 
