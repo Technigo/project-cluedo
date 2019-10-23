89bbb2699c918059f5460e1439cec7608a68448c
AceTircevski <47758694+AceTircevski@users.noreply.github.com>
2019-10-23 12:45:36 +0200

    created objects for suspects,weapon

--- code/clue.js
+++ code/clue.js
@@ -8,16 +8,104 @@ const mrGreen = {
   age: 45,
   image: "assets/green.png",
   occupation: "Entrepreneur"
-}
+};
 
+const mrsWhite = {
+  firstName: "mrs",
+  lastName: "white",
+  color: "white",
+  description: "She is a good actress",
+  age: 63,
+  image: "assets/white.png",
+  occupation: "Actress"
+};
+
+const profPlum = {
+  firstName: "Victor",
+  lastName: "Plum",
+  color: "purple",
+  description: "He is quick witted",
+  age: 43,
+  image: "assets/green.png",
+  occupation: "Professor"
+};
+const  missScarlet = {
+  firstName: "Cassandra",
+  lastName: "Scarlet",
+  color: "red",
+  description: "She is smart",
+  age: 40,
+  image: "assets/green.png",
+  occupation: "Fashion designer"
+};
+const mrMustard = {
+  firstName: "Colonel",
+  lastName: "Mustard",
+  color: "yellow",
+  description: "He is dangerous",
+  age: 39,
+  image: "assets/green.png",
+  occupation: "Military"
+};
+const mrsPeacock = {
+  firstName: "Eleanor",
+  lastName: "Peacock",
+  color: "blue",
+  description: "She is verbal",
+  age: 26,
+  image: "assets/green.png",
+  occupation: "Author"
+};
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: "rope",
   weight: 10
-}
+};
+
+const rope = {
+  name: "knife",
+  weight: 10
+};
+
+const rope = {
+  name: "Candlestick",
+  weight: 10
+};
+
+const rope = {
+  name: "Dumbbell",
+  weight: 10
+};
+
+const rope = {
+  name: "Poison",
+  weight: 10
+};
+
+const rope = {
+  name: "Axe",
+  weight: 10
+};
+
+const rope = {
+  name: "Bat",
+  weight: 10
+};
+
+const rope = {
+  name: "Trophy",
+  weight: 10
+};
+
+const rope = {
+  name: "Pistol",
+  weight: 10
+};
+
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
 
@@ -28,16 +116,16 @@ const suspects = [
   missScarlet,
   mrsPeacock,
   mrMustard
-]
+];
 
-const weapons = []
+const weapons = [];
 
-const rooms = []
+const rooms = [];
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
-  return array[Math.floor(Math.random() * array.length)]
-}
+  return array[Math.floor(Math.random() * array.length)];
+};
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
@@ -47,16 +135,16 @@ const randomSelector = array => {
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
+  mystery.killer = randomSelector(suspects);
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
-  const theKiller = document.getElementById("killer")
-  const theKillerName = document.getElementById("killerName")
+  const theKiller = document.getElementById("killer");
+  const theKillerName = document.getElementById("killerName");
 
-  theKiller.style.background = mystery.killer.color
+  theKiller.style.background = mystery.killer.color;
   theKillerName.innerHTML =
-    mystery.killer.firstName + " " + mystery.killer.lastName
-}
+    mystery.killer.firstName + " " + mystery.killer.lastName;
+};
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
