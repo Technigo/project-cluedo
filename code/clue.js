d6b3dd15cd32c31383d9845ce7987d213db231f7
elol42 <elin.olsson@teliacompany.com>
2019-10-23 18:51:36 +0200

    step2

--- code/clue.js
+++ code/clue.js
@@ -1,5 +1,7 @@
 // CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
 
+
+
 const mrGreen = {
   firstName: "Jacob",
   lastName: "Green",
@@ -8,104 +10,104 @@ const mrGreen = {
   age: 45,
   image: "assets/green.png",
   occupation: "Entrepreneur"
-},
+}
 
-const profrPlum = {
+const profPlum = {
   firstName: "Victor",
   lastName: "Plum",
   color: "purple",
   description: "He has a lot of connections",
-  age: 45,
-  image: "assets/green.png",
-  occupation: "Entrepreneur"
-},
+  age: 47,
+  image: "assets/plum.png",
+  occupation: "Scientist"
+}
 
 const mrsPeacock = {
   firstName: "Eleanor",
   lastName: "Peacock",
   color: "blue",
-  description: "He has a lot of connections",
-  age: 45,
-  image: "assets/green.png",
-  occupation: "Entrepreneur"
-},
+  description: "She knows how to get what shw wants",
+  age: 43,
+  image: "assets/peacock.png",
+  occupation: "Manager"
+}
 
 const missScarlet = {
   firstName: "Cassandra",
   lastName: "Scarlet",
   color: "red",
-  description: "He has a lot of connections",
-  age: 45,
-  image: "assets/green.png",
-  occupation: "Entrepreneur"
-},
+  description: "She really know how to make a stew",
+  age: 39,
+  image: "assets/scarlet.png",
+  occupation: "Chef"
+}
 
-const colonelMustard = {
+const mrMustard = {
   firstName: "Jack",
   lastName: "Mustard",
   color: "yellow",
-  description: "He has a lot of connections",
-  age: 45,
-  image: "assets/green.png",
-  occupation: "Entrepreneur"
-},
+  description: "He likes to hunt",
+  age: 57,
+  image: "assets/mustard.png",
+  occupation: "Kapten"
+}
 
 const mrsWhite = {
   firstName: "Mrs",
   lastName: "White",
   color: "White",
-  description: "He has a lot of connections",
-  age: 45,
-  image: "assets/green.png",
-  occupation: "Entrepreneur"
-},
+  description: "She can really clean",
+  age: 65,
+  image: "assets/white.png",
+  occupation: "Housekeeper"
+}
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: "rope",
   weight: 10
-},
+}
 
 const knife = {
   name: "knife",
   weight: 12
-},
+}
 
 const candelstick = {
   name: "Candelstick",
   weight: 9
-},
+}
 
 const dumbell = {
   name: "Dumbbell",
   weight: 8
-},
+}
 
 const poison = {
   name: "Poison",
   weight: 11
-},
+}
 
 const axe = {
   name: "Axe",
   weight: 13
-},
+}
 
 const bat = {
   name: "bat",
   weight: 9
-},
+}
 
 const trophy = {
   name: "Trophy",
   weight: 7
-},
+}
 
 const pistol = {
   name: "Pistol",
   weight: 11
-},
+}
 
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
@@ -121,6 +123,13 @@ const suspects = [
   mrMustard
 ]
 
+const writeOutsuspect = suspect => {
+  console.log(suspects);
+};
+
+writeOutsuspect(suspects);
+
+
 const weapons = [
   rope,
   knife,
@@ -133,23 +142,36 @@ const weapons = [
   pistol
 ]
 
+const writeOutWeapon = weapon => {
+  console.log(weapons);
+};
+
+writeOutWeapon(weapons);
+
+
 const rooms = [
-  dinningRoom,
-  conservatory,
-  kitchen,
-  study,
-  library,
-  billiardRoom,
-  lounge,
-  ballRoom,
-  hall,
-  spa,
-  livingRoom,
-  observatory,
-  theatre,
-  guestHouse,
-  patio
+  "dinningRoom",
+  "conservatory",
+  "kitchen",
+  "study",
+  "library",
+  "billiardRoom",
+  "lounge",
+  "ballRoom",
+  "hall",
+  "spa",
+  "livingRoom",
+  "observatory",
+  "theatre",
+  "guestHouse",
+  "patio"
 ]
+
+const writeOutroom = room => {
+  console.log(rooms);
+};
+
+writeOutWeapon(rooms);
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
@@ -159,6 +181,11 @@ const randomSelector = array => {
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
+//suspects, weapons, rooms
+
+let mystery = () => {
+
+}
 
 // FINISH THIS FUNCTION TO SHOW ALL INFORMATION ABOUT THE KILLER.
 // This function will be invoked when you click on the killer card.
@@ -178,4 +205,4 @@ const pickKiller = () => {
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
-// 'The murder was committed by Jacob Green, in the living room with a rope.'
+// The murder was committed by Jacob Green, in the living room with a rope.
