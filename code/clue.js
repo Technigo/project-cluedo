de7ad53d9e8b9211bf884e082f4ac82a407c9211
Theresa Ulwahn <theresaulwahn@gmail.com>
2019-10-24 00:36:00 +0200

    iteration 3 done and alert too, but without revealing correct killer

--- code/clue.js
+++ code/clue.js
@@ -1,5 +1,4 @@
-// CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS: The suspects Mr Green - Jacob Green Professor Plum - Victor Plum Miss Scarlet - Cassandra Scarlet Mrs Peacock - Eleanor Peacock Colonel Mustard - Jack Mustard Mrs White - Mrs White
-
+// OBJECTS FOR ALL THE SUSPECTS, 
 const mrGreen = {
   firstName: "Jacob",
   lastName: "Green",
@@ -19,6 +18,7 @@ const profPlum = {
   image: "assets/plum.png",
   occupation: "Professor"
 }
+
 const missScarlet = {
   firstName: "Cassandra",
   lastName: "Scarlet",
@@ -28,6 +28,7 @@ const missScarlet = {
   image: "assets/scarlet.png",
   occupation: "Hacker"
 }
+
 const mrsPeacock = {
   firstName: "Eleanor",
   lastName: "Peacock",
@@ -37,15 +38,17 @@ const mrsPeacock = {
   image: "assets/peacock.png",
   occupation: "Doctor"
 }
+
 const colonelMustard = {
   firstName: "Jack",
   lastName: "Mustard",
-  color: "brown",
+  color: "orange",
   description: "He is good with weapons",
   age: 47,
   image: "assets/mustard.png",
   occupation: "Colonel"
 }
+
 const mrsWhite = {
   firstName: "Mrs",
   lastName: "White",
@@ -57,48 +60,49 @@ const mrsWhite = {
 };
 
 
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE. The Weapons Rope Knife Candlestick Dumbbell Poison Axe Bat Trophy Pistol
+// OBJECTS FOR ALL THE WEAPONS
 
-const rope = {
-  name: "rope",
+const Rope = {
+  name: "Rope",
   weight: 10
 }
-const knife = {
-  name: "knife",
+const Knife = {
+  name: "Knife",
   weight: 2
 }
-const candlestick = {
-  name: "candlestick",
+const Candlestick = {
+  name: "Candlestick",
   weight: 1
 }
-const dumbbell = {
-  name: "dumbbell",
+const Dumbbell = {
+  name: "Dumbbell",
   weight: 40
 }
-const poison = {
-  name: "poison",
+const Poison = {
+  name: "Poison",
   weight: 60
 }
-const axe = {
-  name: "axe",
+const Axe = {
+  name: "Axe",
   weight: 50
 }
-const bat = {
-  name: "bat",
+const Bat = {
+  name: "Bat",
   weight: 15
 }
-const trophy = {
-  name: "trophy",
+const Trophy = {
+  name: "Trophy",
   weight: 3
 }
-const pistol = {
-  name: "pistol",
+const Pistol = {
+  name: "Pistol",
   weight: 90
 }
 
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE. The Rooms Dinning Room Conservatory Kitchen Study Library Billiard Room Lounge Ballroom Hall Spa Living Room Observatory Theater Guest House Patio
+// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE. 
 
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
+// ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS. 
+// No Quatations on objects in arrays
 
 const suspects = [
   mrGreen,
@@ -108,23 +112,20 @@ const suspects = [
   mrsPeacock,
   colonelMustard
 ]
-
   console.log(suspects);
 
-
 const weapons = [
-  rope, 
-  knife,
-  candlestick,
-  dumbbell,
-  poison, 
-  axe, 
-  bat,
-  trophy, 
-  pistol
+  Rope, 
+  Knife,
+  Candlestick,
+  Dumbbell,
+  Poison, 
+  Axe, 
+  Bat,
+  Trophy, 
+  Pistol
 ]
   console.log(weapons);
-
 
 const rooms = [
   "DiningRoom", 
@@ -147,52 +148,87 @@ const rooms = [
 
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
-const randomSelectorSuspects = (suspectsGroup) => {
-  return suspectsGroup[Math.floor(Math.random() * suspectsGroup.length)];
-};
+const randomSelector = array => {
+  return array[Math.floor(Math.random() * array.length)]
+}
 
-const randomSelectorWeapons = (weaponsGroup) => {
-  return weaponsGroup[Math.floor(Math.random() * weaponsGroup.length)];
-};
-
-//const randomSelectorRooms = (roomsGroup) => {
-  //return roomsGroup[Math.floor(Math.random() * roomsGroup.length)];
-//};
-
-console.log(randomSelectorSuspects(suspects).firstName + " " + randomSelectorSuspects(suspects).lastName); 
-//console.log(randomSelectorWeapons(weapons).name);
-//console.log(randomSelectorWeapons(rooms));
+//Checking with console log that they are random
+console.log(randomSelector(suspects).firstName + " " + randomSelector(suspects).lastName); 
+console.log(randomSelector(weapons).name);
+console.log(randomSelector(rooms));
 
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
+
+const mystery = {
+  killer: "Mrs White",
+  color: "white",
+  image: "assets/white.png",
+  weapon: "Dumbbell",
+  room: "Spa"
+};
+
 // FINISH THIS FUNCTION TO SHOW ALL INFORMATION ABOUT THE KILLER.
 // This function will be invoked when you click on the killer card.
-//const pickKiller = () => {
-  //mystery.killer = randomSelector(suspects); // This assigns a random suspect to the mystery.
-  
 
-  
-  //const theKiller = document.getElementById("killer"); // This selects the HTML element that should get colorized
-  
 
- 
-  //theKiller.style.background = mystery.killer.color 
-//}  
-  
 // This sets the color of the card to the killer's color (Note: Ensure that the color can be set as CSS property. For example, "mustard" is not a color, but "orange" is
 
   const pickKiller = () => {
-    //This will randomly select a killer from the suspects. And add that to the mystery object.
 
-    mystery.killer = randomSelectorSuspects(suspects)
+    //This will randomly select a killer from the suspects. And add that to the mystery object.
+    mystery.killer = randomSelector(suspects)
 
     // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
-    
-    const theKiller = document.getElementById("killer")
+    // This selects the HTML element that should get colorized
+
+    const theKiller = document.getElementById("killer") 
     const theKillerName = document.getElementById("killerName")
+    
     theKiller.style.background = mystery.killer.color
     theKillerName.innerHTML =
       mystery.killer.firstName + " " + mystery.killer.lastName
   }
+
+  // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+
+  const pickWeapon = () => {
+
+    mystery.weapon = randomSelector(weapons)
+
+    const theWeapon = document.getElementById("weapon")
+    const theWeaponName = document.getElementById("weaponName")
+    
+    theWeapon.style.background = mystery.weapon.color
+    theWeaponName.innerHTML =
+      mystery.weapon.name + " " + mystery.weapon.weight
+
+  }
+
+  const pickRoom = () => {
+
+    mystery.room = randomSelector(rooms)
+
+    //const theRoom = document.getElementById("room")
+    const theRoomName = document.getElementById("roomName")
+    
+    theRoomName.style.background = mystery.room.color
+    theRoomName.innerHTML = mystery.room
+  }
+
+
+// CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
+// 'The murder was committed by Jacob Green, in the living room with a rope.'
+
+
+  //invoking the function revealMystery and sends alert with confirmation 
+const revealMystery = (theKiller) => {
+  alert (`The murder was committed by " + mysteryKiller + ", in the " + mystery.room +" "+ " with a " + mystery.weapon`);
+  }
+  
+  //orderConfirmation is sent to the HTML to write it out on the site, connecting with ID from HTML
+  document.getElementById
+  ("mysteryRevealText").innerHTML = (`The murder was committed by ${mystery.killer}, in the ${mystery.room} with ${mystery.weapon}!`
+    );
