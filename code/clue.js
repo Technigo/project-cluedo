8c68cda8abb036b79b742d312b483ceacf0db9eb
Agnes Ketesdi <agnes@Agness-MacBook-Air.local>
2019-10-23 15:30:05 +0200

    step 1 and 2 done

--- code/clue.js
+++ code/clue.js
@@ -8,14 +8,104 @@ const mrGreen = {
   age: 45,
   image: "assets/green.png",
   occupation: "Entrepreneur"
-}
+};
+
+const colMustard = {
+  firstName: "Jack",
+  lastName: "Mustard",
+  color: "yellow",
+  description: "A dignified, dapper and dangerous military man",
+  age: 62,
+  image: "assets/mustard.png",
+  occupation: "Colonel"
+};
+
+const mrsPeacock = {
+  firstName: "Eleanor",
+  lastName: "Peacock",
+  color: "blue",
+  description: "Lady of Arlington Grange and a beautiful society hostess",
+  age: 32,
+  image: "assets/peacock.png",
+  occupation: "Senator"
+};
+
+const profPlum = {
+  firstName: "Victor",
+  lastName: "Plum",
+  color: "purple",
+  description: "A quick-witted, young professor with a bow tie and glasses",
+  age: 43,
+  image: "assets/plum.png",
+  occupation: "Professor"
+};
+
+const missScarlet = {
+  firstName: "Cassandra",
+  lastName: "Scarlet",
+  color: "red",
+  description: "A stock femme fatale, young, cunning and attractive",
+  age: 36,
+  image: "assets/scarlet.png",
+  occupation: "Actress"
+};
+
+const mrsWhite = {
+  firstName: "Blanche",
+  lastName: "White",
+  color: "white",
+  description: "An intrusive and kind maid",
+  age: 54,
+  image: "assets/white.png",
+  occupation: "Maid"
+};
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: "rope",
   weight: 10
-}
+};
+
+const knife = {
+  name: "knife",
+  weight: 4
+};
+
+const candleStick = {
+  name: "candlestick",
+  weight: 7
+};
+
+const dumbbell = {
+  name: "dumbbell",
+  weight: 4
+};
+
+const poison = {
+  name: "poison",
+  weight: 1
+};
+
+const axe = {
+  name: "axe",
+  weight: 15
+};
+
+const bat = {
+  name: "bat",
+  weight: 16
+};
+
+const trophy = {
+  name: "trophy",
+  weight: 20
+};
+
+const pistol = {
+  name: "pistol",
+  weight: 12
+};
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
@@ -23,21 +113,47 @@ const rope = {
 
 const suspects = [
   mrGreen,
-  mrsWhite,
   profPlum,
   missScarlet,
   mrsPeacock,
-  mrMustard
-]
+  colMustard,
+  mrsWhite
+];
 
-const weapons = []
+const weapons = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+];
 
-const rooms = []
+const rooms = [
+  dinningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballRoom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+];
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
-  return array[Math.floor(Math.random() * array.length)]
-}
+  return array[Math.floor(Math.random() * array.length)];
+};
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
@@ -47,16 +163,16 @@ const randomSelector = array => {
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
+  mystery.killer = randomSelector(suspects);
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer.
-  const theKiller = document.getElementById("killer")
-  const theKillerName = document.getElementById("killerName")
+  const theKiller = document.getElementById("killer");
+  const theKillerName = document.getElementById("killerName");
 
-  theKiller.style.background = mystery.killer.color
+  theKiller.style.background = mystery.killer.color;
   theKillerName.innerHTML =
-    mystery.killer.firstName + " " + mystery.killer.lastName
-}
+    mystery.killer.firstName + " " + mystery.killer.lastName;
+};
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
