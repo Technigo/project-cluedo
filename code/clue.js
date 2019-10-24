e71c897f53fada6a7a5b8f17fcc4a46cb4c8fbee
elol42 <elin.olsson@teliacompany.com>
2019-10-24 14:48:04 +0200

    fourth iteration

--- code/clue.js
+++ code/clue.js
@@ -276,3 +276,10 @@ const pickRoom = () => {
 
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // The murder was committed by Jacob Green, in the living room with a rope.
+
+const revealMystery = () => {
+  document.getElementById("mystery").innerHTML = `The murder was commit by ${mystery.killer.firstName} ${mystery.killer.lastName}, in ${mystery.room} with a ${mystery.weapon.name}`
+}
+
+//revealMystery.onclick(revealMystery); -kommentar: onclick behövs inte i javascript om onclick finns i HTML på en knapp(button)
+
