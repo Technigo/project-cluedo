0fe6d7e90c2f29a7742953c4f95e0edbba8133c1
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2019-10-25 11:15:23 +0200

    first commit, characters,weapons and rooms

--- code/clue.js
+++ code/clue.js
@@ -9,17 +9,102 @@ const mrGreen = {
   image: "assets/green.png",
   occupation: "Entrepreneur"
 }
+const profPlum = {
+  firstName: "Victor",
+  lastName: "Plum",
+  color: "purple",
+  description: " A quick-witted, young professor with a bow tie",
+  age: 35,
+  image: "assets/plum.png",
+  occupation: "Professor"
+}
+const missScarlet = {
+  firstName: "Cassandra",
+  lastName: "Scarlet",
+  color: "red",
+  description: "An attractive young woman, who allegedly is a femme fatale.",
+  age: 22,
+  image: "assets/scarlet.png",
+  occupation: "Spy"
+}
+const mrsPeacock = {
+  firstName: "Eleonor",
+  lastName: "Peacock",
+  color: "blue",
+  description: "A grande dame, an elderly yet still attractive woman who maintains her dignity in almost all cases.",
+  age: 32,
+  image: "assets/peacock.png",
+  occupation: "Socialite"
+}
+const mrMustard = {
+  firstName: "Jack",
+  lastName: "Mustard",
+  color: "yellow",
+  description: "He is a military man both dignified and dangerous.",
+  age: 50,
+  image: "assets/mustard.png",
+  occupation: "Colonel"
+}
+const mrsWhite = {
+  firstName: "Blanche",
+  lastName: "White",
+  color: "white",
+  description: "A frazzled servant.",
+  age: 62,
+  image: "assets/white.png",
+  occupation: "Colonel"
+}
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: "rope",
+  material: "hamp",
+  weight: 7
+}
+const knife = {
+  name: "knife",
+  material: "steel",
+  weight: 7
+}
+const candlestick = {
+  name: "candelstick",
+  material: "wax",
+  weight: 3
+}
+const dumbbell = {
+  name: "dumbbell",
+  material: "iron",
+  weight: 15
+}
+const poison = {
+  name: "poison",
+  material: "plant",
+  weight: 0.5
+}
+const axe = {
+  name: "axe",
+  material: "iron and wood",
   weight: 10
 }
+const bat = {
+  name: "bat",
+  material: "wood",
+  weight: 8
+}
+const throphy = {
+  name: "throphy",
+  material: "plastic",
+  weight: 2
+}
+const pistol = {
+    name: "pistol",
+    material: "steel",
+    weight: 6
+  }
+  // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
-
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
+// ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
 
 const suspects = [
   mrGreen,
@@ -30,9 +115,35 @@ const suspects = [
   mrMustard
 ]
 
-const weapons = []
+const weapons = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  throphy,
+  pistol
+]
 
-const rooms = []
+const rooms = [
+  dinningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballroom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
