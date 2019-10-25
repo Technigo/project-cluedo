b1180ab69593dd85b4ccbabe63a42869b60843b5
Nina Månsson <ninamansster@gmail.com>
2019-10-25 17:28:58 +0200

    fixing bug in revealMystery-function

--- code/clue.js
+++ code/clue.js
@@ -222,22 +222,20 @@ const pickRoom = () => {
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 
+
+//det här är det som funkar som sammafattande meddelande
 // const revealMystery = () => {
 //   document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName}  ${mystery.killer.lastName} in the ${mystery.room} with a ${mystery.weapon.name}.`
 // }
 
-// //testing
+//här kommer conditional-satsen om man inte valt kort:
 const revealMystery = () => {
   if (!mystery.killer || !mystery.weapon || !mystery.room) {
-    document.getElementById("mystery").innerHTML = "No mystery is yet to be revealed, select three cards first!"
+    document.getElementById("mystery").innerHTML =
+      "No mystery is yet to be revealed, select three cards first!"
   }
   else {
-    mystery.innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room}, ${mystery.time} with ${mystery.weapon.name}.`
+    document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName}  ${mystery.killer.lastName} in the ${mystery.room} with a ${mystery.weapon.name}.`
   }
 }
-
-//det här är det som funkar
-// const revealMystery = () => {
-//   document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName}  ${mystery.killer.lastName} in the ${mystery.room} with a ${mystery.weapon.name}.`
-// }
 
