38e82db727c3ef0f9f52229aa405a018f5ae611a
Jenny Jacobsson <enni@oans.net>
2019-10-25 22:45:50 +0200

    added stretch-goals, responsive design, a new deck of cards

--- code/clue.js
+++ code/clue.js
@@ -11,6 +11,16 @@ const mrGreen = {
   occupation: "Entrepreneur"
 };
 
+const mrFluffy = {
+  firstName: "Grumpy",
+  lastName: "Cat",
+  color: "black",
+  description: "He just grumpy",
+  age: 5,
+  image: "assets/cat.jpg",
+  occupation: "Cat"
+};
+
 const professorPlum = {
   firstName: "Victor",
   lastName: "Plum",
@@ -66,80 +76,152 @@ const mrsWhite = {
 const rope = {
   name: "Rope",
   weight: 10,
-  image: "assets/rope.png"
+  image: "assets/weapons/rope.png"
 };
 
 const knife = {
   name: "Knife",
   weight: 20,
-  image: "assets/knife.png"
+  image: "assets/weapons/knife.png"
 };
 
 const candlestick = {
   name: "Candlestick",
   weight: 5,
-  image: "assets/candlestick.png"
+  image: "assets/weapons/candlestick.png"
 };
 
 const dumbbell = {
   name: "Dumbbell",
   weight: 30,
-  image: "assets/dumbbell.png"
+  image: "assets/weapons/dumbbell.png"
 };
 
 const poison = {
   name: "Poison",
   weight: 2,
-  image: "assets/poison.png"
+  image: "assets/weapons/poison.png"
 };
 
 const axe = {
   name: "Axe",
   weight: 80,
-  image: "assets/axe.png"
+  image: "assets/weapons/axe.png"
 };
 
 const bat = {
   name: "Bat",
   weight: 25,
-  image: "assets/bat.png"
+  image: "assets/weapons/bat.png"
 };
 
 const trophy = {
   name: "Trophy",
   weight: 45,
-  image: "assets/trophy.png"
+  image: "assets/weapons/trophy.png"
 };
 
 const pistol = {
   name: "Pistol",
   weight: 29,
-  image: "assets/gun.png"
+  image: "assets/weapons/gun.png"
 };
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
-const rooms = [
-  "Dinning Room",
-  "Conservatory",
-  "Kitchen",
-  "Study",
-  "Library",
-  "Billiard Room",
-  "Lounge",
-  "Ballroom",
-  "Hall",
-  "Spa",
-  "Living Room",
-  "Observatory",
-  "Theater",
-  "Guest House",
-  "Patio"
-];
+const dinningRoom = {
+  name: "Dinning Room",
+  image: "assets/rooms/living-room.png"
+};
+
+const conservatory = {
+  name: "Conservatory",
+  image: "assets/rooms/conservatory.png"
+};
+
+const kitchen = {
+  name: "Kitchen",
+  image: "assets/rooms/kitchen.png"
+};
+
+const study = {
+  name: "Study",
+  image: "assets/rooms/study.png"
+};
+
+const library = {
+  name: "Library",
+  image: "assets/rooms/study.png"
+};
+
+const billiardRoom = {
+  name: "Billiard Room",
+  image: "assets/rooms/living-room.png"
+};
+
+const lounge = {
+  name: "Lounge",
+  image: "assets/rooms/study.png"
+};
+
+const ballroom = {
+  name: "Ballroom",
+  image: "assets/rooms/study.png"
+};
+
+const hall = {
+  name: "Hall",
+  image: "assets/rooms/hall.png"
+};
+
+const spa = {
+  name: "Spa",
+  image: "assets/rooms/spa.png"
+};
+const livingRoom = {
+  name: "Living Room",
+  image: "assets/rooms/living-room.png"
+};
+
+const observatory = {
+  name: "Observatory",
+  image: "assets/rooms/study.png"
+};
+
+const theater = {
+  name: "Theater",
+  image: "assets/rooms/study.png"
+};
+
+const guestHouse = {
+  name: "Guest House",
+  image: "assets/rooms/guest-house.png"
+};
+
+const patio = {
+  name: "Patio",
+  image: "assets/rooms/study.png"
+};
+
+const morning = {
+  name: "in the break of dawn",
+  image: "assets/time.png"
+};
+
+const day = {
+  name: "at noon",
+  image: "assets/time.png"
+};
+
+const night = {
+  name: "in the evening",
+  image: "assets/time.png"
+};
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
 
 const suspects = [
   mrGreen,
+  mrFluffy,
   mrsWhite,
   professorPlum,
   missScarlet,
@@ -158,6 +240,26 @@ const weapons = [
   trophy,
   pistol
 ];
+
+const rooms = [
+  dinningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballroom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+];
+
+const times = [morning, day, night];
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
@@ -179,9 +281,11 @@ const pickKiller = () => {
   const theKiller = document.getElementById("killer");
   const theKillerName = document.getElementById("killerName");
   const theKillerImage = document.getElementById("killerImage");
+  const thekillerOccupation = document.getElementById("killerOccupation");
 
   theKiller.style.background = mystery.killer.color;
   theKillerImage.src = mystery.killer.image;
+  thekillerOccupation.innerHTML = mystery.killer.occupation;
   theKillerName.innerHTML =
     mystery.killer.firstName + " " + mystery.killer.lastName;
 };
@@ -202,23 +306,33 @@ const pickRoom = () => {
   mystery.room = randomSelector(rooms);
   const theRoom = document.getElementById("room");
   const theRoomName = document.getElementById("roomName");
+  const theRoomImage = document.getElementById("roomImage");
 
   // theRoom.style.background = mystery.room.color;
-  theRoomName.innerHTML = mystery.room;
+  theRoomName.innerHTML = mystery.room.name;
+  theRoomImage.src = mystery.room.image;
+};
+
+const pickTime = () => {
+  mystery.time = randomSelector(times);
+  const theTime = document.getElementById("times");
+  const theTimeName = document.getElementById("timeName");
+  const theTimeImage = document.getElementById("timeImage");
+
+  theTimeName.innerHTML = mystery.time.name;
+  theTimeImage.src = mystery.time.image;
 };
 
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 const revealMystery = () => {
-  if (!mystery.killer || !mystery.room || !mystery.weapon) {
+  if (!mystery.killer || !mystery.room || !mystery.weapon || !mystery.time) {
     document.getElementById("mystery").innerHTML =
-      '<img src="https://media.giphy.com/media/RaLIOPl8MLyWA/source.gif" />';
+      '<img src="https://media.giphy.com/media/26DN6iTo6WZoROvDi/source.gif" />';
   } else {
     document.getElementById(
       "mystery"
-    ).innerHTML = `The murder was comitted by ${mystery.killer.firstName} ${
-      mystery.killer.lastName
-    } in the ${mystery.room || "void"} with a ${mystery.weapon.name}.`;
+    ).innerHTML = `<p>The murder was comitted by ${mystery.killer.firstName} ${mystery.killer.lastName} in the ${mystery.room.name}, ${mystery.time.name},  with a ${mystery.weapon.name}.</p>`;
   }
 };
 
