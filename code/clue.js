4b2b13f600703aab21aa609d379d9040a7c73709
ucha3i <jchabik@gmail.com>
2019-10-26 16:31:13 +0200

    step 1 & 2

--- code/clue.js
+++ code/clue.js
@@ -71,7 +71,7 @@ const suspects = [
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS THE THE WEAPONS IF YOU LIKE.
 
-const rope = [
+const weapons = [
 
     {
         name: "rope",
@@ -149,19 +149,18 @@ const rooms = [
 ]
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS.
-/* 
-const suspects = [
+
+/* const suspects = [
     mrGreen,
     mrsWhite,
     profPlum,
     missScarlet,
     mrsPeacock,
     mrMustard
-]
+] */
 
-const weapons = []
+/* const weapons = [] */
 
- */
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 const randomSelector = array => {
@@ -171,6 +170,12 @@ const randomSelector = array => {
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
+
+const mystery {
+    killer,
+    weapon,
+    room
+}
 
 // FINISH THIS FUNCTION TO SHOW ALL INFORMATION ABOUT THE KILLER.
 // This function will be invoked when you click on the killer card.
