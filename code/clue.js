98f53d4337ca6f6272c609fda814065d37bde709
Petra B <petrabrinck@icloud.com>
2019-10-26 14:42:36 +0200

    final commit

--- code/clue.js
+++ code/clue.js
@@ -5,7 +5,7 @@ const mrGreen = {
   lastName: "Green",
   color: "Green",
   description: "He has a lot of connections",
-  age: 39,
+  age: 49,
   image: "assets/green.png",
   occupation: "Entrepreneur"
 };
@@ -14,8 +14,8 @@ const profPlum = {
   firstName: "Victor",
   lastName: "Plum",
   color: "purple",
-  description: "He is a small fat man",
-  age: 56,
+  description: "He is a tiny confused man",
+  age: 42,
   image: "assets/plum.png",
   occupation: "Lawyer"
 };
@@ -24,7 +24,7 @@ const missScarlet = {
   firstName: "Cassandra",
   lastName: "Scarlet",
   color: "orange",
-  description: "She is a tall intelligent woman",
+  description: "Tall intelligent woman",
   age: 33,
   image: "assets/scarlet.png",
   occupation: "Tech Entrepreneur"
@@ -34,10 +34,10 @@ const mrsPeacock = {
   firstName: "Eleanor",
   lastName: "Peacock",
   color: "blue",
-  description: "She devious elderly woman",
-  age: 56,
+  description: "A devious person",
+  age: 53,
   image: "assets/peacock.png",
-  occupation: "Media woman"
+  occupation: "News anchor"
 };
 
 const colonelMustard = {
@@ -54,10 +54,10 @@ const mrsWhite = {
   firstName: "Alice",
   lastName: "White",
   color: "white",
-  description: "A sweet and caring young woman",
-  age: 24,
+  description: "A sweet and caring woman",
+  age: 68,
   image: "assets/white.png",
-  occupation: "Nurse at County General Hospital Chicago "
+  occupation: "Nurse"
 };
 
 
@@ -65,56 +65,65 @@ const mrsWhite = {
 
 const rope = {
   name: "Rope",
-  weight: 10,
-  color: "black",
+  weight: 1.4,
+  material: "Natural fiber",
+  image: "weapon/rope.jpg" //IMG-länken till funktionen på rad 257
 }
 
 const knife = {
   name: "Knife",
-  weight: 25,
-  color: "brown",
+  weight: 0.3,
+  material: "Steel and wood",
+  image: "weapon/knife.jpg"
 }
 
 const candlestick = {
   name: "Candlestick",
-  weight: 20,
-  color: "grey",
+  weight: 1.2,
+  material: "Brass",
+  image: "weapon/candlestick.jpg"
 }
 
 const dumbbell = {
   name: "Dumbbell",
-  weight: 40,
-  color: "pink",
+  weight: 20,
+  material: "Steel and vinyl",
+  image: "weapon/dumbbell.jpg"
 }
 
 const poison = {
   name: "Poison",
-  weight: 2,
-  color: "darkgrey",
+  weight: 0.4,
+  material: "Green fluid",
+  image: "weapon/poison.jpg"
 }
 
 const axe = {
   name: "Axe",
   weight: 35,
-  color: "lightpink",
+  material: "Wood and steel",
+  image: "weapon/axe.jpg"
 }
 
 const bat = {
   name: "Bat",
-  weight: 35,
-  color: "lightblue",
+  weight: 1.5,
+  material: "Metal",
+  image: "weapon/bat.jpg"
 }
 
 const trophy = {
   name: "Trophy",
-  weight: 60,
-  color: "yellow",
+  weight: 2.5,
+  material: "Metal and wood",
+  image: "weapon/trophy.jpg"
 }
 
 const pistol = {
   name: "Pistol",
-  weight: 30,
-  color: "darkgreen",
+  weight: 0.8,
+  material: "Aluminium",
+  image: "weapon/pistol.jpg"
 }
 
 const weaponsImage = {
@@ -173,7 +182,18 @@ const rooms = [
   "Patio"
 ];
 
-console.log(rooms);
+console.log(room);
+
+const times = [
+  "afternoon",
+  "midnight",
+  "morning",
+  "evening",
+  "dawn",
+  "dusk"
+];
+
+console.log(times);
 
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FOR THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
@@ -184,24 +204,18 @@ const randomSelector = array => {
 
 console.log(randomSelector(suspects).firstName + "" + (suspects).lastName);
 console.log(randomSelector(weapons).name);
-console.log(randomSelector(rooms));
+console.log(randomSelector(room));
+
 
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
 
-const theMystery = {
-  killer: "mrs Peacock",
-  color: "blue",
-  image: "assets/peacock.png",
-  weapon: "trophy",
-  room: "theatre"
-};
+
 
 // This will randomly select a killer from the suspects. And add that to the mystery object.
 // FINISH THIS FUNCTION TO SHOW ALL INFORMATION ABOUT THE KILLER.
-// This function will be invoked when you click on the killer card.
 
 const pickKiller = () => {
   mystery.killer = randomSelector(suspects)
@@ -213,8 +227,8 @@ const pickKiller = () => {
   theKillerName.innerHTML =
     mystery.killer.firstName + " " + mystery.killer.lastName
 
-  /// REMAINING OBJECT INFO ///
 
+  // REMAINING OBJECT INFO //
   const theKillerAge = document.getElementById("killerAge")
   theKillerAge.innerHTML =
     mystery.killer.age
@@ -232,9 +246,8 @@ const pickKiller = () => {
 
 }
 
-
-// CREATE FUNCTIONS 
-// weapon //
+// FUNCTIONS //
+// weapons //
 const pickWeapon = () => {
   mystery.weapon = randomSelector(weapons)
 
@@ -244,31 +257,46 @@ const pickWeapon = () => {
   const theWeaponWeight = document.getElementById("weaponWeight")
   theWeaponWeight.innerHTML = mystery.weapon.weight
 
-  const theWeaponImage = document.getElementById("weaponImage")
-  heWeaponImage.src = mystery.weapon.image
+  const theWeaponMaterial = document.getElementById("weaponMaterial")
+  theWeaponMaterial.innerHTML = mystery.weapon.material
 
-  //const theWeaponColor = document.getElementById("weaponColor")
-  //theWeaponColor.innerHTML = mystery.weapon.color
+  //Weapon images // 
+  const theWeaponImage = document.getElementById("weaponImage")
+  theWeaponImage.src = mystery.weapon.image
 }
 
-// room //
+// rooms //
 const pickRoom = () => {
   mystery.room = randomSelector(rooms)
 
   const theRoomName = document.getElementById("roomName");
   theRoomName.innerHTML = mystery.room
 }
+
+// time of murder //
+const pickTime = () => {
+  mystery.time = randomSelector(times)
+
+  const theTimeName = document.getElementById("timeName");
+  theTimeName.innerHTML = mystery.time
+
+
+}
+
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 
-// Invoking the function reveal the mystery and send alert //
-function revealMystery() {
-  alert(`The murder was committed by mrs Peacock,
-with a trophe in the theatre`)
+const revealMystery = () => {
+  const theReveal = document.getElementById("mystery")
+
+  theReveal.innerHTML = (`The murder was committed by ${mysteryKiller.name} with a ${mysteryKiller.weapon} in the ${mysteryKiller.room} at ${mysteryKiller.time}.`);
 };
 
-// on click -------- something is sent to the HTML file that makes it show in the DOM incl id from html
-//document.getElementById(
-  //"mystery"
-//).innerHTML = (`The murder was committed by ${mystery.killer}, with a ${mystery.weapon} in the ${mystery.room}`)
-//revealMystery(theKiller);
+const mysteryKiller = {
+  name: "mrs Peacock",
+  image: "assets/peacock.png",
+  weapon: "trophy",
+  room: "observatory",
+  time: "midnight"
+};
+
