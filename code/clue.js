3fc4b886f4208c3960f8f74871a45594a6ca29a7
HannaCharlotteHolm <hanna.charlotte.holm@gmail.com>
2019-10-26 16:48:13 +0200

    updated CSS

--- code/clue.js
+++ code/clue.js
@@ -237,3 +237,10 @@ const pickRoom = () => {
 
 // CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+const revealMystery = () => {
+
+  const theMystery = document.getElementById("mystery")
+
+  theMystery.innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with the ${mystery.weapon.name}`
+
+}
