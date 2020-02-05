7a5badfbc4e47dda29378f2d046a14cc79eeb507
sara <sara.lovens.lovn@gmail.com>
2020-02-05 15:47:51 +0100

    console log

--- code/clue.js
+++ code/clue.js
@@ -9,13 +9,16 @@ const mrGreen = {
   image: 'assets/green.png',
   occupation: 'Entrepreneur'
 }
-
+console.log(mrGreen.description)
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: 'rope',
   weight: 10
 }
+
+console.log(rope.weight)
+
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
