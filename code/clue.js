8054d72d34b4b8030935c169a4743f1679a48257
Mathias Thorström <thisen@gmail.com>
2020-02-06 22:09:21 +0100

    refactored JS, get all the random functions work

--- code/clue.js
+++ code/clue.js
@@ -49,22 +49,6 @@ const cluedoMurders = {
   }
 }
 
-// Store firstName + lastName from our object to suspects array
-suspects = []
-
-const getMurderNames = (obj) => {
-  for ( let i in obj ) {
-    suspects.push(`${obj[i].firstName} ${obj[i].lastName}`)
-  }
-}
-
-getMurderNames(cluedoMurders)
-
-// Check if suspects array are populated
-// console.log(suspects)
-
-
-
 // Weapons
 const cluedoWeapons = {
   rope : {
@@ -179,55 +163,67 @@ const cluedoRooms = {
   }
 }
 
-const weapons = []
-const rooms = []
+suspects = []
+// Return array of target object
+suspects = Object.entries(cluedoMurders)
+// Check that suspects is populated
+// console.log(suspects)
+// console.log(suspects[0][1].firstName + ' ' + suspects[0][1].lastName)
+// console.log(`${suspects[0][1].firstName} ${suspects[0][1].lastName} favorite color is ${suspects[0][1].color}`)
 
-const getNames = (obj, arr) => {
-  for ( let i in obj ) {
-    arr.push(`${obj[i].name}`)
-  }
-}
+weapons = []
+// Return array of target object
+weapons = Object.entries(cluedoWeapons)
+// Check that weapons is populated
+// console.log(weapons[0][1].name)
+// console.log(`${weapons[0][1].name} weights ${weapons[0][1].weight} ${weapons[0][1].weightUnit}`)
 
-getNames(cluedoWeapons, weapons)
-getNames(cluedoRooms, rooms)
-
-// Check if weapons and rooms arrays are populated
-console.log(weapons)
-console.log(rooms)
-
-
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
-
-// const suspects = [
-//   mrGreen,
-//   mrsWhite
-//   // ...  and the rest
-// ]
+rooms = []
+// Return array of target object
+rooms = Object.entries(cluedoRooms)
 
 
-// THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
-// YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
 const randomSelector = array => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
-// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
-// With a killer, a weapon and a room.
-// The values will be set later.
-
+const mystery = {}
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects)
+  console.log(mystery.killer)
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
-  document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById(
-    'killerName'
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+  // document.getElementById('killerCard').style.background = mystery.killer.color
+  document.getElementById('killerName').innerHTML = `${mystery.killer[1].firstName} ${mystery.killer[1].lastName}`
+}
+
+const pickWeapon = () => {
+  // This will randomly select a killer from the suspects. And add that to the mystery object.
+  mystery.weapon = randomSelector(weapons)
+  console.log(mystery.weapon)
+
+  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
+  // document.getElementById('weaponcard').style.background = mystery.killer.color
+  document.getElementById('weaponName').innerHTML = `${mystery.weapon[1].name}`
+}
+
+const pickRoom = () => {
+  // This will randomly select a killer from the suspects. And add that to the mystery object.
+  mystery.room = randomSelector(rooms)
+  console.log(mystery.room)
+
+  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
+  // document.getElementById('roomCard').style.background = mystery.killer.color
+  document.getElementById('roomName').innerHTML = `${mystery.room[1].name}`
 }
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+const clickBait = () => {
+  element = document.getElementById("mystery")
+  element.innerHTML = `${mystery.killer[1].firstName} ${mystery.killer[1].lastName} ${mystery.weapon[1].name} ${mystery.room[1].name}`
+}
