24dc2b17f312a956ea61933e01668808e0729143
Christina Persson <christina@Christinas-MacBook.local>
2020-02-07 09:31:22 +0100

    step 4 done, i think

--- code/clue.js
+++ code/clue.js
@@ -60,7 +60,7 @@ const mrsWhite = {
   occupation: "Accountant"
 };
 
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
+// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE..
 
 const rope = {
   name: "rope",
@@ -160,6 +160,12 @@ const randomSelector = array => {
 // With a killer, a weapon and a room.
 // The values will be set later.
 
+const mystery = {
+  killer: "",
+  weapon: "",
+  room: ""
+};
+
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
@@ -170,9 +176,35 @@ const pickKiller = () => {
   document.getElementById(
     "killerName"
   ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+
+  const killerImage = document.getElementById("killerImage");
+  killerImage.src = mystery.killer.image;
 };
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+const pickWeapon = () => {
+  mystery.weapon = randomSelector(weapons);
+
+  document.getElementById("weaponName").innerHTML = `${mystery.weapon.name}`;
+  document.getElementById(
+    "weaponWeight"
+  ).innerHTML = `${mystery.weapon.weight}`;
+};
+
+const pickRoom = () => {
+  mystery.room = randomSelector(rooms);
+  document.getElementById("roomName").innerHTML = `${mystery.room}`;
+};
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+
+const revealMystery = mystery => {
+  document.getElementById(
+    "mystery"
+  ).innerHTML = `The murder was comitted by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}`;
+};
+
+document.getElementById("roomCard").onclick = pickRoom;
+document.getElementById("weaponCard").onclick = pickWeapon;
+document.getElementById("killerCard").onclick = pickKiller;
