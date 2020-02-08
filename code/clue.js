fe9a65f689771b67de650e5351852f0786f62612
palhamel <pal.hamel@gmail.com>
2020-02-08 20:29:26 +0100

    step 1 done

--- code/clue.js
+++ code/clue.js
@@ -10,12 +10,103 @@ const mrGreen = {
   occupation: 'Entrepreneur'
 }
 
+const profPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  color: 'purple',
+  description: 'He has made controversial claims over Egyptian history.',
+  age: 36,
+  image: 'assets/plum.png',
+  occupation: 'Professor'
+}
+
+const missScarlet = {
+  firstName: 'Cassandra',
+  lastName: 'Scarlet',
+  color: 'red',
+  description: 'She will do anything to stay in the spotlight.',
+  age: 25,
+  image: 'assets/scarlet.png',
+  occupation: 'Actress'
+}
+
+const mrsPeacock = {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  color: 'blue',
+  description: 'She is a formidable politician.',
+  age: 32,
+  image: 'assets/peacock.png',
+  occupation: 'Socialite'
+}
+
+const colMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  color: 'yellow',
+  description: 'He is a retired military man.',
+  age: 55,
+  image: 'assets/mustard.png',
+  occupation: 'Colonel'
+}
+
+const mrsWhite = {
+  firstName: 'Mrs',
+  lastName: 'White',
+  color: 'white',
+  description: 'She will do anything for justice, including turning vigilante.',
+  age: 60,
+  image: 'assets/peacock.png',
+  occupation: 'Housekeeper'
+}
+
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: 'rope',
-  weight: 10
+  weight: 10,
 }
+
+const knife = {
+  name: 'knife',
+  weight: 15
+}
+
+const candleStick = {
+  name: 'candlestick',
+  weight: 20
+}
+
+const dumbBell = {
+  name: 'dumbbell',
+  weight: 50
+}
+
+const poison = {
+  name: 'poison',
+  weight: 7
+}
+
+const axe = {
+  name: 'axe',
+  weight: 100
+}
+
+const bat = {
+  name: 'bat',
+  weight: 80
+}
+
+const trophy = {
+  name: 'trophy',
+  weight: 30
+}
+
+const pistol = {
+  name: 'pistol',
+  weight: 90
+}
+
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
@@ -23,13 +114,49 @@ const rope = {
 
 const suspects = [
   mrGreen,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  colMustard,
   mrsWhite
-  // ...  and the rest
 ]
 
-const weapons = []
 
-const rooms = []
+const weapons = [
+  rope,
+  knife,
+  candleStick,
+  dumbBell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+]
+
+const rooms = [
+  'Dining Room',
+  'Conservatory',
+  'Kitchen',
+  'Study Room',
+  'Library',
+  'Billiard Room',
+  'Lounge Area',
+  'Ballroom',
+  'Hall',
+  'Spa',
+  'Living Room',
+  'Observatory',
+  'Theater',
+  'Guest House',
+  'Patio'
+]
+// ************************ REMOVE this ***************************
+// Check objects/arrays. 
+// console.log(suspects, weapons, rooms);
+// console.log(typeof suspects)
+// console.log(rooms);
+// ************************ REMOVE this *************************** 
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
