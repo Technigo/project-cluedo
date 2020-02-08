f36c79594d8cfd130a18e3064f9e1059f15d9b0f
camilla <camilla.gausland@gmail.com>
2020-02-08 17:21:00 +0100

    created functions for pickRoom and pickWeapon

--- code/clue.js
+++ code/clue.js
@@ -136,7 +136,7 @@ const randomSelector = array => {
 // With a killer, a weapon and a room.
 // The values will be set later.
 const mystery = {
-  killer:'',
+  killer: '',
   weapon: '',
   room: ''
 }
@@ -155,6 +155,22 @@ const pickKiller = () => {
 }
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+
+const pickWeapon = () => {
+  mystery.weapon =randomSelector(weapons)
+  document.getElementById('weaponCard').style.bacground = mystery.weapon.color
+  document.getElementById(
+    'weaponName'
+  ).innerHTML = `${mystery.weapon.name} ${mystery.weapon.weight} kg`
+}
+
+const pickRoom = () => {
+  mystery.room =randomSelector(rooms)
+  document.getElementById('roomCard').style.background = mystery.room.color
+  document.getElementById(
+    'roomName'
+  ).innerHTML = `${mystery.rooms}`
+}
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
