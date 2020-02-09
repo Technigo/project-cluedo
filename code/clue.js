225beac9cd71b41212a39ee9113ad996656ef965
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-02-09 12:53:10 +0100

    added the objects and created the arrays

--- code/clue.js
+++ code/clue.js
@@ -10,6 +10,57 @@ const mrGreen = {
   occupation: 'Entrepreneur'
 }
 
+const mrsWhite = {
+  firstName: 'Grace',
+  lastName: 'White',
+  color: 'white',
+  description: 'She is wealthy',
+  age: 82,
+  image: 'assets/white.png',
+  occupation: 'Housewife'
+}
+
+const mrMustard = {
+  firstName: 'James',
+  lastName: 'Mustard',
+  color: 'yellow',
+  description: 'He is not who you think he is',
+  age: 59,
+  image: 'assets/mustard.png',
+  occupation: 'Captain'
+}
+
+const msPeacock = {
+  firstName: 'Jane',
+  lastName: 'Peacock',
+  color: 'light green',
+  description: 'She talkes a lot',
+  age: 48,
+  image: 'assets/peacock.png',
+  occupation: 'Shop owner'
+
+}
+
+const drPlum = {
+  firstName: 'Christopher',
+  lastName: 'Plum',
+  color: 'purple',
+  description: 'He is afraid of the dark',
+  age: 53,
+  image: 'assets/plum.png',
+  occupation: 'Medical doctor'
+}
+
+const msScarlet = {
+  firstName: 'Rita',
+  lastName: 'Scarlet',
+  color: 'red',
+  description: 'She is a master of seduction',
+  age: 25,
+  image: 'assets/scarlet.png',
+  occupation: 'Dancer'
+}
+
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
 const rope = {
@@ -17,19 +68,63 @@ const rope = {
   weight: 10
 }
 
+const knife = {
+  name: 'knife',
+  weight: 50
+}
+
+const poison = {
+  name: 'poison'
+  weight: 15
+}
+
+const axe = {
+  name: 'axe'
+  weight: 70
+}
+
+const plasticBag = {
+  name: 'plastic bag'
+  weight: 4
+}
+
+const machete = {
+  name: 'machete'
+  weight: 65
+}
+
+
+
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
 
 const suspects = [
   mrGreen,
-  mrsWhite
-  // ...  and the rest
+  mrsWhite,
+  mrMustard,
+  msPeacock,
+  drPlum,
+  msScarlet
 ]
 
-const weapons = []
+const weapons = [
+  rope,
+  knife,
+  poison,
+  axe,
+  plasticBag,
+  machete
+]
 
-const rooms = []
+const rooms = [
+  kitchen,
+  livingroom,
+  hallway,
+  bathroom,
+  basement,
+  bedroom
+]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
