5f0dfcb6b12fb3fe5ef9765e9a3a93cb328e24cc
Olle Alm <olle.alm@me.com>
2020-02-09 23:59:21 +0100

    Step 1

--- code/clue.js
+++ code/clue.js
@@ -9,11 +9,91 @@ const mrGreen = {
   image: 'assets/green.png',
   occupation: 'Entrepreneur'
 }
+const profPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+}
+const missScarlet = {
+  firstName: 'Cassandra',
+  lastName: 'Scarlet',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+}
+const mrsPeacock = {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+}
+const colMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  color: 'yellow',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+}
+const mrsWhite = {
+  firstName: 'Mrs',
+  lastName: 'White',
+  color: 'white',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+}
+
+
+
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: 'rope',
+  weight: 10
+}
+const knife = {
+  name: 'knife',
+  weight: 10
+}
+const candlestick = {
+  name: 'candlestick',
+  weight: 10
+}
+const dumbbell = {
+  name: 'dumbbell',
+  weight: 10
+}
+const poison = {
+  name: 'poison',
+  weight: 10
+}
+const axe = {
+  name: 'axe',
+  weight: 10
+}
+const bat = {
+  name: 'bat',
+  weight: 10
+}
+const trophy = {
+  name: 'trophy',
+  weight: 10
+}
+const pistol = {
+  name: 'pistol',
   weight: 10
 }
 
@@ -23,13 +103,39 @@ const rope = {
 
 const suspects = [
   mrGreen,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  colMustard,
   mrsWhite
   // ...  and the rest
 ]
 
-const weapons = []
+const weapons = [rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol]
 
-const rooms = []
+const rooms = ["Dining Room",
+  "Conservatory",
+  "Kitchen",
+  "Study",
+  "Library",
+  "Billiard Room",
+  "Lounge",
+  "Ballroom",
+  "Hall",
+  "Spa",
+  "Living Room",
+  "Observatory",
+  "Theater",
+  "Guest House",
+  "Patio"]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
@@ -41,12 +147,20 @@ const randomSelector = array => {
 // With a killer, a weapon and a room.
 // The values will be set later.
 
+let mystery = {
+  killer: "",
+  weapon: "",
+  room: ""
+}
+
+
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects)
 
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
+  // This will change the background color of the card to the one connected to the chosen killer 
+  // and show the full name of the killer. Feel free to add more things to show about the killer.
   document.getElementById('killerCard').style.background = mystery.killer.color
   document.getElementById(
     'killerName'
@@ -55,5 +169,8 @@ const pickKiller = () => {
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
-// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
+// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. 
+// It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+
+console.log(mrGreen.description)
