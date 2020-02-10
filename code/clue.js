cdb66d3cc589965aa4e113e0ee828e3256d79d16
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-02-10 14:38:40 +0100

    Iteration 3 and 4 done

--- code/clue.js
+++ code/clue.js
@@ -203,8 +203,8 @@ const pickRoom = () => {
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 const revealMystery = () => {
-  console.log(`The murder was committed by ${mystery.killer.firstName}, in the ${mystery.room} with a ${mystery.weapon.name}.`)
-document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName}, in the ${mystery.room} with a ${mystery.weapon.name}.`
+  console.log(`The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}.`)
+document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}.`
 }
 
 document.getElementById("button").onclick = revealMystery
