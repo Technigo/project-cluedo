2ea3d28b520ced5c51dd48dc9eed0428daf2c6a8
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-02-10 16:10:57 +0100

    Added styling

--- code/clue.js
+++ code/clue.js
@@ -1,9 +1,10 @@
 // STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
 
 const mrGreen = {
+  nickName: 'Mr Green',
   firstName: 'Jacob',
   lastName: 'Green',
-  color: 'green',
+  color: '#48878d',
   description: 'He has a lot of connections',
   age: 45,
   image: 'assets/green.png',
@@ -11,9 +12,10 @@ const mrGreen = {
 };
 
 const professorPlum = {
+  nickName: 'Professor Plum',
   firstName: 'Peter',
   lastName: 'Plum',
-  color: 'purple',
+  color: '#928eb5',
   description: 'A man with a degree of suspicion.',
   age: 36,
   image: 'assets/plum.png',
@@ -21,9 +23,10 @@ const professorPlum = {
 };
 
 const missScarlet = {
+  nickName: 'Miss Scarlet',
   firstName: 'Vivienne',
   lastName: 'Scarlet',
-  color: 'red',
+  color: '#d0676b',
   description: 'A vivacious and aspiring actress.',
   age: 25,
   image: 'assets/scarlet.png',
@@ -31,9 +34,10 @@ const missScarlet = {
 };
 
 const mrsPeacock = {
+  nickName: 'Mrs Peacock',
   firstName: 'Elizabeth',
   lastName: 'Peacock',
-  color: 'blue',
+  color: '#9eb4db',
   description: 'Lady of Arlington Grange and a beautiful society hostess.',
   age: 32,
   image: 'assets/peacock.png',
@@ -41,9 +45,10 @@ const mrsPeacock = {
 };
 
 const colonelMustard = {
+  nickName: 'Colonel Mustard',
   firstName: 'Michael',
   lastName: 'Mustard',
-  color: 'yellow',
+  color: '#ccb064',
   description: 'A gallant military hero whose glittering career hides a tarnished past.',
   age: 50,
   image: 'assets/mustard.png',
@@ -51,9 +56,10 @@ const colonelMustard = {
 };
 
 const mrsWhite = {
+  nickName: 'Mrs White',
   firstName: 'Blanche',
   lastName: 'White',
-  color: 'white',
+  color: '#E8DFD6',
   description: 'Loyal housekeeper and a devoted confidant.',
   age: 60,
   image: 'assets/white.png',
@@ -64,47 +70,56 @@ const mrsWhite = {
 
 const rope = {
   name: 'Rope',
-  weight: 10
+  weight: 5,
+  description: 'Cotton rope.'
 };
 
 const knife = {
   name: 'Knife',
-  weight: 5
+  weight: 2,
+  description: 'Rusty kitchen knife.'
 };
 
 const candlestick = {
   name: 'Candlestick',
-  weight: 15
+  weight: 5,
+  description: 'Heavy silver candlestick.'
 };
 
 const dumbbell = {
   name: 'Dumbbell',
-  weight: 20
+  weight: 20,
+  description: 'Perfect for exercise.'
 };
 
 const poison = {
   name: 'Poison',
-  weight: 3
+  weight: 1,
+  description: 'Cyanide or maybe Arsenic?'
 };
 
 const axe = {
   name: 'Axe',
-  weight: 30
+  weight: 20,
+  description: 'Perfect for cutting wood.'
 };
 
 const bat = {
   name: 'Bat',
-  weight: 20
+  weight: 10,
+  description: 'Baseball bat.'
 };
 
 const trophy = {
   name: 'Trophy',
-  weight: 20
+  weight: 20,
+  description: 'Trophy that needs polishing.'
 };
 
 const pistol = {
   name: 'Pistol',
-  weight: 10
+  weight: 2,
+  description: 'One missing bullet in this one.'
 };
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
@@ -170,41 +185,39 @@ const mystery = {
 
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
-  document.getElementById('killerImage')
+ 
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects)
+  document.getElementById('killerImage').src = `${mystery.killer.image}`
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
   document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById(
-    'killerName'
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+  document.getElementById('killerNickname').innerHTML = `${mystery.killer.nickName}`
+  document.getElementById('killerName').innerHTML = `Full Name: ${mystery.killer.firstName} ${mystery.killer.lastName}`
+  document.getElementById('killerOccupation').innerHTML = `Occupation: ${mystery.killer.occupation}`
+  document.getElementById('killerAge').innerHTML = `Age: ${mystery.killer.age}`
+  document.getElementById('killerDescription').innerHTML = `${mystery.killer.description}`
 };
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 const pickWeapon = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.weapon = randomSelector(weapons)
-  document.getElementById(
-    'weaponName'
-  ).innerHTML = `${mystery.weapon.name}`
-  document.getElementById(
-    'weaponWeight'
-  ).innerHTML = `${mystery.weapon.weight}`
+  document.getElementById('weaponName').innerHTML = `${mystery.weapon.name}`
+  document.getElementById('weaponWeight').innerHTML = `${mystery.weapon.weight} kg`
+  document.getElementById('weaponDescription').innerHTML = `${mystery.weapon.description}`
 };
 
 const pickRoom = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.room = randomSelector(rooms)
-  document.getElementById(
-    'roomName'
-  ).innerHTML = `${mystery.room}`
+  document.getElementById('roomName').innerHTML = `${mystery.room}`
 };
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 const revealMystery = () => {
   console.log(`The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}.`)
-document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}.`
+  document.getElementById("mystery").innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}.`
 }
 
 document.getElementById("button").onclick = revealMystery
