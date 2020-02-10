c3bc8f556851c974e7d18630a89723ab90b2ba2a
elsareutersward <elsa.reutersward@gmail.com>
2020-02-10 10:45:12 +0100

    Step 1 and Step 2 - complete

---
+++ clue.js
@@ -0,0 +1,186 @@
+// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
+
+const mrGreen = {
+  firstName: 'Jacob',
+  lastName: 'Green',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 45,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+}
+
+const professorPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  color: 'purple',
+  description: 'He is a quickwitted, intellectual',
+  age: 36,
+  image: 'assets/plum.png',
+  occupation: 'Professor'
+}
+
+const missScarlet = {
+  firstName: 'Cassandra',
+  lastName: 'Scarlet',
+  color: 'red',
+  description: 'She is young, cunning and ambitious',
+  age: 27,
+  image: 'assets/scarlet.png',
+  occupation: 'Actress'
+}
+
+const mrsPeacock = {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  color: 'blue',
+  description: 'She knows all the socialite gossip',
+  age: 58,
+  image: 'assets/peacock.png',
+  occupation: 'Socialite'
+}
+
+const colonelMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  color: 'yellow',
+  description: 'He is a dignified, dapper and dangerous military man',
+  age: 62,
+  image: 'assets/mustard.png',
+  occupation: 'Colonel'
+}
+
+const mrsWhite = {
+  firstName: 'Mrs',
+  lastName: 'White',
+  color: 'white',
+  description: 'She has worked for the family all her life',
+  age: 75,
+  image: 'assets/white.png',
+  occupation: 'Housekeeper'
+}
+
+// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
+
+const rope = {
+  name: 'rope',
+  weight: 10
+}
+
+const knife = {
+  name: 'knife',
+  weight: 10
+}
+
+const candlestick = {
+  name: 'candlestick',
+  weight: 10
+}
+
+const dumbbell = {
+  name: 'dumbbell',
+  weight: 10
+}
+
+const poison = {
+  name: 'poison',
+  weight: 10
+}
+
+const axe = {
+  name: 'axe',
+  weight: 10
+}
+
+const bat = {
+  name: 'bat',
+  weight: 10
+}
+
+const trophy = {
+  name: 'trophy',
+  weight: 10
+}
+
+const pistol = {
+  name: 'pistol',
+  weight: 10
+}
+
+// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+
+// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
+
+const suspects = [
+  mrGreen,
+  professorPlum,
+  missScarlet,
+  mrsPeacock,
+  colonelMustard,
+  mrsWhite 
+]
+
+const weapons = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+]
+
+const rooms = [
+  'diningRoom', 
+  'conservatory', 
+  'kitchen', 
+  'study', 
+  'library', 
+  'billiardRoom', 
+  'lounge', 
+  'ballroom', 
+  'hall', 
+  'spa', 
+  'livingRoom', 
+  'observatory', 
+  'theater', 
+  'guestHouse', 
+  'patio'
+]
+
+// THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
+// YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
+const randomSelector = array => {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
+// With a killer, a weapon and a room.
+// The values will be set later.
+
+const mystery = {
+  killer: suspects,
+  weapon: weapons,
+  room: rooms,
+}
+
+// This function will be invoked when you click on the killer card.
+const pickKiller = () => {
+  // This will randomly select a killer from the suspects. And add that to the mystery object.
+  mystery.killer = randomSelector(suspects)
+
+  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
+  document.getElementById('killerCard').style.background = mystery.killer.color
+  document.getElementById(
+    'killerName'
+  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+}
+
+// CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+}
+
+
+// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
+// 'The murder was committed by Jacob Green, in the living room with a rope.'
