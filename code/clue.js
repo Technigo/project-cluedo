d720fb0fecfa1ef49b57f3e11fb4d20fe38e185d
Petra Jud <petrajud@Petras-Air-2.lan>
2020-02-12 13:56:42 +0100

    step 3 done

--- code/clue.js
+++ code/clue.js
@@ -1,5 +1,3 @@
-// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
-
 const mrGreen = {
   firstName: 'Jacob',
   lastName: 'Green',
@@ -51,76 +49,77 @@ const colMustard = {
 }
 
 const mrsWhite = {
-  firstName: "",
+  firstName: 'Mrs',
   lastName: 'White',
-  color: 'white',
+  color: 'lightgrey',
   description: 'She has been with the household for over 30 years.',
   age: 57,
   image: 'assets/white.png',
   occupation: 'Housekeeper'
 }
 
-
-
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
-
 const rope = {
   name: 'Rope',
   weight: 3,
-  twoHanded: true
+  twoHanded: true,
+  color: 'darkblue',
 }
 
 const knife = {
-  name: 'knife',
+  name: 'Knife',
   weight: 2,
-  twoHanded: false
+  twoHanded: false,
+  color: 'darkgreen'
 }
 
 const candlestick = {
   name: 'Candlestick',
   weight: 5,
-  twoHanded: false
+  twoHanded: false,
+  color: 'gold'
 }
 
 const dumbbell = {
   name: 'Dumbbell',
   weight: 10,
-  twoHanded: false
+  twoHanded: false,
+  color: 'blue'
 }
 
 const poison = {
   name: 'Poison',
   weight: 1,
-  twoHanded: true
+  twoHanded: true,
+  color: 'orange'
 }
 
 const axe = {
   name: 'Axe',
   weight: 8,
   twoHanded: true,
+  color: 'red'
 }
 
 const bat = {
   name: 'Bat',
   weight: 2,
   twoHanded: true,
+  color: 'brown'
 }
 
 const trophy = {
   name: 'Trophy',
   weight: 4,
   twoHanded: false,
+  color: 'gold'
 }
 
 const pistol = {
   name: 'Pistol',
   weight: 1,
   twoHanded: false,
+  color: 'purple'
 }
-
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
-
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
 
 const suspects = [
   mrGreen,
@@ -161,30 +160,49 @@ const rooms = [
   "Patio"
 ]
 
-
-// THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
-// YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
 const randomSelector = array => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
-// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
-// With a killer, a weapon and a room.
-// The values will be set later.
+let mystery = {
+  killer: "",
+  weapon: "",
+  room: ""
+}
 
-// This function will be invoked when you click on the killer card.
 const pickKiller = () => {
-  // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects)
-
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
   document.getElementById('killerCard').style.background = mystery.killer.color
   document.getElementById(
     'killerName'
   ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+  document.getElementById('killerImage').src = mystery.killer.image
+  document.getElementById('killerAge').innerHTML = `${mystery.killer.age} years old`
+  document.getElementById('killerOccupation').innerHTML = mystery.killer.occupation
+  document.getElementById('killerDescription').innerHTML = mystery.killer.description
 }
 
-// CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+const pickWeapon = () => {
+  mystery.weapon = randomSelector(weapons)
+  document.getElementById('weaponCard').style.background = mystery.weapon.color
+  document.getElementById('weaponName').innerHTML = mystery.weapon.name
+  if (mystery.weapon.twoHanded === false) {
+    document.getElementById('weaponWeight').innerHTML = `It weighs ${mystery.weapon.weight} kg and is a one-handed weapon.`
+  }
+  else if (mystery.weapon.twoHanded === true) {
+    document.getElementById('weaponWeight').innerHTML = `It weighs ${mystery.weapon.weight} kg and is a two-handed weapon.`
+  }
+  
+}
 
+const pickRoom = () => {
+  mystery.room = randomSelector(rooms)
+  document.getElementById('roomName').innerHTML = mystery.room
+}
+
+
+document.getElementById('killerCard').onclick = pickKiller
+document.getElementById('weaponCard').onclick = pickWeapon
+document.getElementById('roomCard').onclick = pickRoom
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
