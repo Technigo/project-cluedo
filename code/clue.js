bfff186e385f4b3d0a83e6b5f7c70bc59e264570
Petra Jud <petrajud@Petras-Air-2.lan>
2020-02-12 12:28:11 +0100

    step 2 done

--- code/clue.js
+++ code/clue.js
@@ -161,8 +161,6 @@ const rooms = [
   "Patio"
 ]
 
-console.log(mrGreen.description)
-console.log(rope.twoHanded)
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
