d166eec4495e57efd8d1c7823e045085f85d7622
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-09-13 19:59:22 +0200

    Second step

--- code/clue.js
+++ code/clue.js
@@ -82,6 +82,11 @@ const dumbbell = {
   weight: 150,
 };
 
+const poison = {
+  name: "poison",
+  weight: 5,
+};
+
 const axe = {
   name: "axe",
   weight: 85,
@@ -141,13 +146,46 @@ console.log(knife.name);
 
 const suspects = [
   mrGreen,
+  professorPlum,
+  missScarlet,
+  mrsPeacock,
+  colonelMustard,
   mrsWhite,
-  // ...  and the rest
 ];
 
-const weapons = [];
+const weapons = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol,
+];
 
-const rooms = [];
+const rooms = [
+  diningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballRoom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio,
+];
+
+console.log(rooms);
+console.log(weapons);
+console.log(suspects);
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
