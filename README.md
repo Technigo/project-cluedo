9a52266a5b1d99421ce1a32bc173cf733dff303c
Emelie Svensson <emeliesv@live.se>
2020-09-14 10:32:49 +0200

    Added objects into arrays suspects, weapons and room

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Replace this readme with your own information about your project.
 
 Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
 
+This is a javascript Murder Mystery game.
+The assignment was to create a cluedo-game consisting of three cards with suspects and weapons.
+
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
--- code/clue.js
+++ code/clue.js
@@ -1,41 +1,136 @@
 // STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
 
-const mrGreen = {
-  firstName: 'Jacob',
-  lastName: 'Green',
-  color: 'green',
-  description: 'He has a lot of connections',
-  age: 45,
-  image: 'assets/green.png',
-  occupation: 'Entrepreneur'
-}
-
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
-
-const rope = {
-  name: 'rope',
-  weight: 10
-}
-
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
-
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
-
 const suspects = [
-  mrGreen,
-  mrsWhite
-  // ...  and the rest
-]
+  {
+    firstName: "Jacob",
+    lastName: "Green",
+    color: "green",
+    description: "He has a lot of connections",
+    age: 45,
+    image: "assets/green.png",
+    occupation: "Entrepreneur",
+  },
+  {
+    firstName: "Cassandra",
+    lastName: "Scarlet",
+    color: "red",
+    description: "She has a temper",
+    age: 32,
+    image: "assets/scarlet.png",
+    occupation: "Writer",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    color: "purple",
+    description: "He is easily offended",
+    age: 37,
+    image: "assets/plum.png",
+    occupation: "Professor in detergents",
+  },
+  {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    color: "blue",
+    description: "She is very good at solving problems",
+    age: 46,
+    image: "assets/peacock.png",
+    occupation: "Entrepreneur",
+  },
+  {
+    firstName: "Jack",
+    lastName: "Mustard",
+    color: "yellow",
+    description: "He is known to hold a grudge",
+    age: 68,
+    image: "assets/mustard.png",
+    occupation: "Colonel",
+  },
+  {
+    firstName: "Mrs",
+    lastName: "White",
+    color: "white",
+    description:
+      "Has been working in the house for as long as anyone can remember",
+    age: "unknown",
+    image: "assets/white.png",
+    occupation: "Housekeeper",
+  },
+];
 
-const weapons = []
+const weapons = [
+  {
+    name: "rope",
+    weight: 3,
+    description: "worn out",
+  },
+  {
+    name: "knife",
+    weight: 2,
+    description: "brand new",
+  },
+  {
+    name: "candlestick",
+    weight: 4,
+    description: "polished, but old",
+  },
+  {
+    name: "dumbbell",
+    weight: 22,
+    description: "rusty",
+  },
+  {
+    name: "poison",
+    weight: 1,
+    description: "small bottle containing rests of transparent liquid",
+  },
+  {
+    name: "axe",
+    weight: 7,
+    description: "has a worn handle, blade has a tear",
+  },
+  {
+    name: "bat",
+    weight: 7,
+    description: "long, wooden bat with a small crack on handle",
+  },
+  {
+    name: "trophy",
+    weight: 16,
+    description:
+      "heavy, brass trophy with a marble pedestal and inscription on bottom",
+  },
+  {
+    name: "pistol",
+    weight: 8,
+    description: "old Astra model 903, heavy thing",
+  },
+];
 
-const rooms = []
+const rooms = [
+  {
+    name: "Dining room",
+    description: "Long, dark room lighted with candlelights. A big dining table and 12 chairs sits in the middle. Body was found under the table, feet sticking out under the white linen cloth.",
+  },
+  {
+    name: "Conservatory",
+    description: "Big glass,conservatory filled with mediterrainian plants. The body was found on a bed of roses.",
+  },
+  {
+    name: "Theater",
+    description: "The red velvet curtains drapering the stage was a perfect disguise for a body",
+  },
+  {
+    name: 'Kitchen',
+    description: 'Filled with sharp knifes and boiling '
+  },
+];
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
-const randomSelector = array => {
-  return array[Math.floor(Math.random() * array.length)]
-}
+const randomSelector = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
@@ -44,14 +139,14 @@ const randomSelector = array => {
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
+  mystery.killer = randomSelector(suspects);
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
-  document.getElementById('killerCard').style.background = mystery.killer.color
+  document.getElementById("killerCard").style.background = mystery.killer.color;
   document.getElementById(
-    'killerName'
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
-}
+    "killerName"
+  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+};
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
