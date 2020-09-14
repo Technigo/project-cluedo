1ff64fd2b683f2a7783c49d8147075d3f060100c
Corneliaryas <hello@corneliaryas.com>
2020-09-14 16:13:28 +0200

    Step 4 - Shows a reveal mystery message

--- code/clue.js
+++ code/clue.js
@@ -200,3 +200,7 @@ document.getElementById('roomCard').onclick = pickRoom;
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+const revealMystery = () => {
+  document.getElementById('mystery').innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}`
+}
+document.getElementsByTagName("button")[0].onclick = revealMystery;
