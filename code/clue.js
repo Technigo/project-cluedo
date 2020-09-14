6675f376623c5f2ac959a01e503f6a0173a49558
Emelie Svensson <emeliesv@live.se>
2020-09-14 14:51:05 +0200

    added images

--- code/clue.js
+++ code/clue.js
@@ -116,55 +116,55 @@ const weapons = [
 const rooms = [
   {
     name: "Dining room",
-    description: "Long, dark room lighted with candlelights. A big dining table and 12 chairs sits in the middle. Body was found under the table, feet sticking out under the white linen cloth.",
+    description: "We enter the Dining room. A long, dark room lighted with candles. A big dining table and 12 chairs sits in the middle. Under the table we see the body, feet sticking out under the white linen cloth.",
   },
   {
     name: "Conservatory",
-    description: "Big glass,conservatory filled with mediterrainian plants. The body was found on a bed of roses.",
+    description: "We enter the Conservatory. It's a big, glass conservatory filled with mediterrainian plants. On a bed of roses the victim rests.. Quite poetic..",
   },
   {
     name: "Theater",
-    description: "To be or not to be. Well, in this case the victim is no more.. The red velvet curtains drapering the stage was a perfect disguise for a body",
+    description: "We enter the Theater. To be or not to be. Well, in this case the victim is no more.. The red velvet curtains drapering the stage is a perfect disguise for a body",
   },
   {
     name: 'Kitchen',
-    description: 'Filled with sharp knifes and boiling pots. The body was found in the pantry, flour spilled everywhere. Such a mess..'
+    description: "We enter the Kitchen. It's filled with sharp knifes and boiling pots. The body is found in the pantry, flour spilled everywhere. Such a mess.."
   },
   {
     name: 'Study',
-    description: 'Bookcases from the floor reaching the ceiling, but all of the books are on the floor. There sure was a scuffle going on here. The result: Body under the desk.'
+    description: 'We enter the Study. Bookcases from the floor reaching the ceiling, but all of the books are on the floor. There sure was a scuffle going on here. The result: Body under the desk.'
   },
   {
     name: 'Library',
-    description: 'Large bookcases covers the wall. A ladder is resting against one bookcase, and in the space between the bookcase and ceiling the body was found. How did the killer get it up there?'
+    description: 'We enter the Library. Large bookcases covers the wall. A ladder is resting against one bookcase, and in the space between the bookcase and ceiling the body is found. How did the killer get it up there?'
   },
   {
     name: 'Billiard room',
-    description: 'A dark, woodpaneled room with a huge billiardtable in the middle. Of course, the body was found lying straight on the table. What a mess..'
+    description: "We enter the Billiard room. It's dark, woodpaneled room with a huge billiardtable in the middle. Of course, the body is found lying straight on the table. What a mess.."
   },
   {
     name: 'Lounge',
-    description: "Velvet couches and a broken glasstable. Sherry is spilled on a dark, blue carpet. Wait a minute.. that's not sherry! A body is sticking out under one of the couches.."
+    description: "We enter the Lounge. Velvet couches and a broken glasstable. Sherry is spilled on a dark, blue carpet. Wait a minute.. that's not sherry! A body is sticking out under one of the couches.."
   },
   {
     name: 'Ballroom',
-    description: "Once there was a ball filled with joy and music. Now there's only tragic, blood and death. The body was found in a corner of the room, the once so shint marble floor, now covered in blood."
+    description: "We enter the Ballroom. Once there was a ball filled with joy and music. Now there's only tragic, blood and death. The body is found in a corner of the room, the once so shiny marble floor is now covered in blood."
   },
   {
-    name: 'Hall',
-    description: 'Creeky floorboards and portraits glaring. The killer must have been very quiet and have known exactly which boards to avoid to not be discovered. The body was hidden behind a big plant, the former lady of the house sneering from her portrait.'
+    name: 'Hallway',
+    description: 'We enter the Hallway. Creeky floorboards and portraits glaring. The killer must have been very quiet and have known exactly which boards to avoid to not be discovered. Behind a big plant we find the body, the former lady of the house sneering from her portrait.'
   },
   {
     name: 'Spa',
-    description: "Soft music playing, the smell of chlorine and moist air. If it wasn't for the body floating in the pool, it would be quite inviting."
+    description: "We enter the Spa. Soft music playing, the smell of chlorine and moist air. If it wasn't for the body floating in the pool, it would be quite inviting."
   },
   {
     name: 'Living Room',
-    description: "With the cracked tv's distorted sound in the background and whiskeybottle on the floor, there are plenty of evidence of a scuffle. The body was carefully placed in a chair in front of the tv."
+    description: "We enter the Living room. With the cracked tv's distorted sound in the background and whiskeybottle on the floor, there are plenty of evidence of a scuffle. The body was carefully placed in a chair in front of the tv."
   },
   {
     name: 'Guest House',
-    description: 'Spartan decor with two small beds. This is far from inviting, black mold covering the corners of the ceiling. The body was found in the closet, maybe the murderer was in a rush?'
+    description: 'We enter the guest house. It has a spartan decor with two small beds. This is far from inviting, black mold covering the corners of the ceiling. The body was found in the closet, maybe the murderer was in a rush?'
   },
   {
     name: 'Patio',
@@ -181,17 +181,25 @@ const randomSelector = (array) => {
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
+const mystery = {
+  killer: "",
+  weapon: "",
+  room: ""
+}
 
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects);
 
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
+  
   document.getElementById("killerCard").style.background = mystery.killer.color;
   document.getElementById(
     "killerName"
   ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+  document.getElementById('killerOccupation').innerHTML = `${mystery.killer.occupation}`
+  document.getElementById('killerImage').src = mystery.killer.image;
+  document.getElementById('killerDescription').innerHTML = `${mystery.killer.description}`;
 };
 
 const pickWeapon = () => {
@@ -213,7 +221,7 @@ const pickRoom = () => {
 
 const revealMystery = () => {
   document.getElementById('messageBox').classList.remove('hidden');
-  document.getElementById('revealMessage').innerHTML = `The murder was commited by ${mystery.killer.name} in the ${mystery.room.name} with the ${mystery.weapon.name}. Scandalous!`;
+  document.getElementById('mystery').innerHTML = `The murder was commited by ${mystery.killer.name} in the ${mystery.room.name} with the ${mystery.weapon.name}. Scandalous!`;
   document.getElementById("roomDescription").innerHTML = `In the darkest hour, while everyone else were busy doing other stuff ${mystery.killer.name} acted quickly. ${mystery.room.description}`;
 };
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
