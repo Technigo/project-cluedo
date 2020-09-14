c49f56769272f0743014491820f0cac9a518a09c
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-14 18:37:13 +0200

    Step 1 done

--- code/clue.js
+++ code/clue.js
@@ -1,5 +1,4 @@
-// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
-
+// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS:
 const mrGreen = {
   firstName: 'Jacob',
   lastName: 'Green',
@@ -8,28 +7,188 @@ const mrGreen = {
   age: 45,
   image: 'assets/green.png',
   occupation: 'Entrepreneur'
-}
+};
 
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
+const professorPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  color: 'plum',
+  description: 'He is a bit whimsical and odd.',
+  age: 53,
+  image: 'assets/plum.png',
+  occupation: 'Professor'
+};
+
+const missScarlet = {
+  firstName: 'Cassandra',
+  lastName: 'Scarlet',
+  color: 'red',
+  description: 'She is strong and mysterious',
+  age: 41,
+  image: 'assets/scarlet.png',
+  occupation: 'Superhero'
+};
+
+const mrsPeacock = {
+  firstName: 'Elanor',
+  lastName: 'Peacock',
+  color: 'blue',
+  description: 'A real socialite.',
+  age: 52,
+  image: 'assets/peacock.png',
+  occupation: 'Headhunter'
+};
+
+const colonelMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  color: 'mustard',
+  description: 'He is fascinated by weapons.',
+  age: 66,
+  image: 'assets/mustard.png',
+  occupation: 'Colonel'
+};
+
+const mrsWhite = {
+  firstName: 'Lily',
+  lastName: 'White',
+  color: 'white',
+  description: 'She is quite strict and knows how to get things done.',
+  age: 68,
+  image: 'assets/white.png',
+  occupation: 'Retired project manager'
+};
+
+//CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
 const rope = {
   name: 'rope',
-  weight: 10
-}
+  weight: 10,
+  flexibility: 3
+};
+
+const knife = {
+  name: 'knife',
+  weight: 1,
+  flexibility: 9
+};
+
+const candleStick = {
+  name: 'candle stick',
+  weight: 2,
+  flexibility: 7
+};
+
+const dumbBell = {
+  name: 'dumbbell',
+  weight: 20,
+  flexibility: 6
+};
+
+const poison = {
+  name: 'poison',
+  weight: 0,
+  flexibility: 10
+};
+
+const axe = {
+  name: 'axe',
+  weight: 12,
+  flexibility: 3
+};
+
+const bat = {
+  name: 'bat',
+  weight: 3,
+  flexibility: 7
+};
+
+const trophy = {
+  name: 'trophy',
+  weight: 5,
+  flexibility: 3
+};
+
+const pistol = {
+  name: 'pistol',
+  weight: 1,
+  flexibility: 9
+};
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+const diningRoom = "Dining Room";
 
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
+const conservatory = "Conservatory";
 
+const kitchen = "Kitchen";
+
+const study = "Study";
+
+const library = "Library";
+
+const billiardRoom = "Billiard Room";
+
+const lounge = "Lounge";
+
+const ballRoom = "Ball Room";
+
+const hall = "Hall";
+
+const spa = "Spa";
+
+const livingRoom = "Living Room";
+
+const observatory = "Observatory";
+
+const theater = "Theater";
+
+const guestHouse = "Guest House";
+
+const patio = "Patio";
+
+//CONSOLE LOG TO TEST:
+console.log(mrsWhite.description);
+console.log(candleStick.flexibility);
+
+// STEP 2. NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
 const suspects = [
   mrGreen,
+  professorPlum,
+  missScarlet,
+  mrsPeacock,
+  colonelMustard,
   mrsWhite
-  // ...  and the rest
 ]
 
-const weapons = []
+const weapons = [
+  rope,
+  knife,
+  candleStick,
+  dumbBell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+]
 
-const rooms = []
+const rooms = [
+  diningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballRoom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
