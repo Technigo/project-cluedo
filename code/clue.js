ad5dfbca2a476cd1124fccea61701580f9333dd4
Gabbi-89 <gabriellabolin@hotmail.com>
2020-09-15 09:28:39 +0200

    Blue level pushed

--- code/clue.js
+++ code/clue.js
@@ -3,7 +3,7 @@
 const mrGreen = {
   firstName: 'Jacob',
   lastName: 'Green',
-  color: 'green',
+  color: '#165209', // Green
   description: 'He has a lot of connections.',
   age: 45,
   image: './assets/green.png',
@@ -13,7 +13,7 @@ const mrGreen = {
 const professorPlum = {
   firstName: 'Victor',
   lastName: 'Plum',
-  color: 'purple',
+  color: '#5a3278', // Purple
   description: 'He is widely seen as the intellectual.',
   age: 36,
   image: './assets/plum.png',
@@ -23,7 +23,7 @@ const professorPlum = {
 const missScarlet = {
   firstName: 'Cassandra',
   lastName: 'Scarlet',
-  color: 'red',
+  color: '#b30e0e', // Red
   description: 'She is a real femme fatale.',
   age: 25,
   image: './assets/scarlet.png',
@@ -33,8 +33,8 @@ const missScarlet = {
 const mrsPeacock = {
   firstName: 'Eleanor',
   lastName: 'Peacock',
-  color: 'blue',
-  description: 'An attractive woman who maintaing her dignity in almost all cases.',
+  color: '#110557', // Blue
+  description: 'She maintains her dignity in almost all cases.',
   age: 32,
   image: './assets/peacock.png',
   occupation: 'Socialite'
@@ -43,7 +43,7 @@ const mrsPeacock = {
 const colonelMustard = {
   firstName: 'Jack',
   lastName: 'Mustard',
-  color: 'yellow',
+  color: '#b3870e', // Mustard yellow
   description: 'A military man both dignified and dangerous.',
   age: 56,
   image: './assets/mustard.png',
@@ -53,7 +53,7 @@ const colonelMustard = {
 const mrsWhite = {
   firstName: 'Mrs',
   lastName: 'White',
-  color: 'white',
+  color: '#f5f3f0', // White
   description: 'She has a grandmotherly appearance.',
   age: 67,
   image: './assets/white.png',
@@ -63,54 +63,49 @@ const mrsWhite = {
 //ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
 const rope = {
-  name: 'rope',
+  name: 'Rope',
   weight: 10
 }
 
 const knife = {
-  name: 'knife',
+  name: 'Knife',
   weight: 18
 }
 
 const candlestick = {
-  name: 'candlestick',
+  name: 'Candlestick',
   weight: 48
 }
 
 const dumbbell = {
-  name: 'dumbbell',
+  name: 'Dumbbell',
   weight: 98
 }
 
 const poison = {
-  name: 'poison',
+  name: 'Poison',
   weight: 3
 }
 
 const axe = {
-  name: 'axe',
+  name: 'Axe',
   weight: 91
 }
 
 const bat = {
-  name: 'bat',
+  name: 'Bat',
   weight: 43
 }
 
 const trophy = {
-  name: 'trophy',
+  name: 'Trophy',
   weight: 33
 }
 
 const pistol = {
-  name: 'pistol',
+  name: 'Pistol',
   weight: 57
 }
-
-// Just testing the above objects with some 'console.log'
-console.log(mrGreen.description);
-console.log(bat.weight);
-console.log(missScarlet.firstName);
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
@@ -138,30 +133,22 @@ const weapons = [
 ]
 
 const rooms = [
-  "diningroom",
-  "conservatory",
-  "kitchen",
-  "studyroom",
-  "libraryroom",
-  "billiardroom",
-  "lounge",
-  "ballroom",
-  "hall",
-  "spa",
-  "livingroom",
-  "observatory",
-  "theater",
-  "guestHouse",
-  "patio"
+  "Dining Room",
+  "Conservatory",
+  "Kitchen",
+  "Study Room",
+  "Library Room",
+  "Billiard Room",
+  "Lounge",
+  "Ball Room",
+  "Hall",
+  "Spa",
+  "Living Room",
+  "Observatory",
+  "Theater",
+  "Guest House",
+  "Patio"
 ]
-
-// Just testing the above arrays with some 'console.log'
-console.log(suspects);
-console.log(weapons);
-console.log(rooms);
-console.log(suspects[2]);
-console.log(rooms[6]);
-
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
@@ -169,60 +156,53 @@ const randomSelector = array => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
-// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
-// With a killer, a weapon and a room.
-// The values will be set later.
-const theMystery = {
-
+// CREATE AN OBJECT THAT KEEPS THE MYSTERY.  With a killer, a weapon and a room. The values will be set later.
+let theMystery = {
+ killer: '',
+ weapon: '',
+ room: ''
 }
 
 const killerImage = document.getElementById('killerImage');
 
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
-  // This will randomly select a killer from the suspects. And add that to the mystery object.
+  // This will randomly select a killer from the suspects and add that to the mystery object.
   mystery.killer = randomSelector(suspects)
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
   document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById(
-    'killerName'
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+  document.getElementById('killerName').innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+  document.getElementById('killerAge').innerHTML = `Age: ${mystery.killer.age}`
+  document.getElementById('killerOccupation').innerHTML = `Occupation: ${mystery.killer.occupation}`
   killerImage.src = mystery.killer.image
+  document.getElementById('killerDescription').innerHTML = `Description: ${mystery.killer.description}`
 }
 document.getElementById('killerCard').onclick = pickKiller
 
 // Function to be invoked when you click on Weapon card - in the similar way as pickKiller.
 const pickWeapon = () => {
   mystery.weapon = randomSelector(weapons)
- // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
- document.getElementById('weaponCard').style.background = mystery.weapon.color // Need to change styling
- document.getElementById(
-   'weaponName'
- ).innerHTML = `${mystery.weapon.name}`
+
+  document.getElementById('weaponName').innerHTML = `${mystery.weapon.name}`
+  document.getElementById('weaponWeight').innerHTML = `Weight: ${mystery.weapon.weight} pounds`
 }
 document.getElementById('weaponCard').onclick = pickWeapon
 
 // Function to be invoked when you click on Room card - in the similar way as pickKiller.
 const pickRoom = () => {
   mystery.room = randomSelector(rooms)
- // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
- document.getElementById('roomCard').style.background = mystery.room.color // Need to change styling
- document.getElementById(
-   'roomName'
- ).innerHTML = `${mystery.room}`
+
+  document.getElementById('roomName').innerHTML = `${mystery.room}`
 }
 document.getElementById('roomCard').onclick = pickRoom
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. 
-// It should show something like:
-// 'The murder was committed by Jacob Green, in the living room with a rope.'
+// It should show something like: 'The murder was committed by Jacob Green, in the living room with a rope.'
+const mysteryImage = document.getElementById('mysteryImage');
+
 revealMystery = () => {
-  mystery.killer = randomSelector(suspects)
-  mystery.weapon = randomSelector(weapons)
-  mystery.room = randomSelector(rooms)
-  document.getElementById('mystery').innerHTML = `The murder was commited by ${mystery.killer.firstName}, in the ${mystery.room} with a ${mystery.weapon.name}.`
-  // console.log(`The murder was commited by ${mystery.killer.firstName}, in the ${mystery.room} with a ${mystery.weapon.name}.`)
-  document.getElementById("killerCard").innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName} <br>Description: ${mystery.killer.description} <br>Age: ${mystery.killer.age} <br>Occupation: ${mystery.killer.occupation} <br>${mystery.killer.image}`
-  document.getElementById("killerCard").style.background = mystery.killer.color
+ 
+  document.getElementById('mystery').innerHTML = `The murder was commited by ${mystery.killer.firstName} ${mystery.killer.lastName} in the ${mystery.room} with a ${mystery.weapon.name}.`
+  
 }
