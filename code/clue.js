69427ca7739d72b31d764b97cd36b285274d9e16
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-15 11:13:24 +0200

    Step one completed

--- code/clue.js
+++ code/clue.js
@@ -60,20 +60,78 @@ const rope = {
   name: 'rope',
   weight: 10
 }
-
+const knife = {
+  name: 'knife',
+  weight: 3
+}
+const candlestick = {
+  name: 'candlestick',
+  weight: 5
+}
+const dumbbell = {
+  name: 'dumbbell',
+  weight: 28
+}
+const poison = {
+  name: 'poison',
+  weight: 1
+}
+const axe = {
+  name: 'axe',
+  weight: 15
+}
+const bat = {
+  name: 'bat',
+  weight: 13
+}
+const trophy = {
+  name: 'trophy',
+  weight: 17
+}
+const pistol = {
+  name: 'pistol',
+  weight: 9
+}
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
 
 const suspects = [
   mrGreen,
-  mrsWhite
-  // ...  and the rest
+  colonelMustard,
+  mrsPeacock,
+  professorPlum,
+  missScarlet,
+  mrsWhite,
 ]
 
-const weapons = []
+const weapons = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol,
+]
 
-const rooms = []
+const rooms = ['Dining Room',
+  'Conservatory',
+  'Kitchen',
+  'Study',
+  'Library',
+  'Billiard Room',
+  'Lounge',
+  'Ballroom',
+  'Hall',
+  'Spa',
+  'Living Room',
+  'Observatory',
+  'Theater',
+  'Guest House',
+  'Patio',]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
