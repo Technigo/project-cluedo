c16315c3a18596d6e7822653b8f6ad995f6aa456
AxelHammarback <axel.hammarback@gmail.com>
2020-09-16 22:33:29 +0200

    Sanity check commit – before fixing the final details

--- code/clue.js
+++ code/clue.js
@@ -42,7 +42,7 @@ const mrsPeacock = {
 const colonelMustard = {
   firstName: 'Jack',
   lastName: 'Mustard',
-  color: '#F0C647',
+  color: '#EE7D2D',
   description: 'Mustard description',
   age: 72,
   image: 'assets/mustard.png',
@@ -52,7 +52,7 @@ const colonelMustard = {
 const mrsWhite = {
   firstName: 'Mrs',
   lastName: 'White',
-  color: '#E9DFCB',
+  color: '#49364A',
   description: 'Mrs white desc',
   age: 83,
   image: 'assets/white.png',
@@ -62,57 +62,66 @@ const mrsWhite = {
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 const rope = {
-  name: 'rope',
-  weight: 10
+  name: 'Snake',
+  weight: 10,
+  emoji: '🐍'
 }
 
 const knife = {
-  name: 'knife',
-  weight: 15
+  name: 'Knife',
+  weight: 15,
+  emoji: '🔪'
 }
 
 const candlestick = {
-  name: 'candle stick',
-  weight: 35
+  name: 'Candle stick',
+  weight: 35,
+  emoji: '🕯'
 }
 
 const dumbbell = {
-  name: 'dumbbell',
-  weight: 150
+  name: 'Dumbbell',
+  weight: 150,
+  emoji: '🏋️‍♀️'
 }
 
 const poison = {
-  name: 'novichok',
-  weight: 5
+  name: 'Novichok',
+  weight: 5,
+  emoji: '🧪'
 }
 
 const axe = {
-  name: 'axe',
-  weight: 85
+  name: 'Axe',
+  weight: 85,
+  emoji: '⛏'
 }
 
 const bat = {
-  name: 'bat',
-  weight: 60
+  name: 'Bat',
+  weight: 60,
+  emoji: '🦇'
 }
 
 const trophy = {
-  name: 'trophy',
-  weight: 75
+  name: 'Trophy',
+  weight: 75,
+  emoji: '🏆'
 }
 
 const pistol = {
-  name: 'pistol',
-  weight: 35
+  name: 'Pistol',
+  weight: 35,
+  emoji: '🔫'
 }
 
 // NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
 const suspects = [
-  mrGreen, 
-  mrsWhite, 
-  professorPlum, 
-  missScarlet, 
-  mrsPeacock, 
+  mrGreen,
+  mrsWhite,
+  professorPlum,
+  missScarlet,
+  mrsPeacock,
   colonelMustard
 ]
 
@@ -127,6 +136,7 @@ const weapons = [
   trophy,
   pistol
 ]
+
 
 const rooms = ['Dining Room', 'Conservatory', 'Kitchen', 'Study', 'Library', 'Billiard Room', 'Lounge', 'Ballroom', 'Hall', 'Spa', 'Living Room', 'Observatory', 'Theater', 'Guest House', 'Patio']
 
@@ -148,7 +158,6 @@ let mystery = {
 
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
-  console.log("Entered function pickKiller.")
   mystery.killer = randomSelector(suspects)
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
@@ -158,12 +167,40 @@ const pickKiller = () => {
 }
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+const pickWeapon = () => {
+  mystery.weapon = randomSelector(weapons)
+  document.getElementById('weaponName').innerHTML = mystery.weapon.name
+  document.getElementById('weaponWeight').innerHTML = "Weight: " + mystery.weapon.weight
+  document.getElementById('weaponEmoji').innerHTML = mystery.weapon.emoji
+}
 
-
+const pickRoom = () => {
+  mystery.room = randomSelector(rooms)
+  document.getElementById('roomName').innerHTML = mystery.room
+}
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+const revealMystery = () => {
+  console.log("Reveal mystery")
+  toggleOverlay()
+  document.getElementById("mysteryRevealText").innerHTML = `The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName} in the ${mystery.room}, using the ${mystery.weapon.name.toLowerCase()}! Huzzah! 🕵🏼‍♀️`
+}
 
-
+const toggleOverlay = () => {
+  const overlay = document.getElementById("fullscreen-cover")
+  if(overlay.style.display === "none") {
+    console.log("1")
+    overlay.style.display = "flex";
+  } else {
+    console.log("2")
+    overlay.style.display = "none";
+  }
+}
 
 document.getElementById('killerCard').onclick = pickKiller
+document.getElementById('weaponCard').onclick = pickWeapon
+document.getElementById('roomCard').onclick = pickRoom
+document.getElementById('revealMystery').onclick = revealMystery
+
+toggleOverlay()
