fdcaf2ee9fc952f55365d0cb0048480f79439709
MindstormingAB <s.elander@mindstorming.se>
2020-09-16 22:37:07 +0200

    Adding variables, validation and reset button

--- code/clue.js
+++ code/clue.js
@@ -60,7 +60,7 @@ const mrsWhite = {
   occupation: 'Waitress'
 }
 
-console.log(msScarlet.occupation);
+// console.log(msScarlet.occupation);
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
 
@@ -109,7 +109,7 @@ const pistol = {
   weight: 16
 }
 
-console.log(dumbbell.weight);
+// console.log(dumbbell.weight);
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
 
@@ -184,15 +184,15 @@ const rooms = [
   patio
 ]
 
-console.log(suspects);
-console.log(weapons);
-console.log(rooms);
+// console.log(suspects);
+// console.log(weapons);
+// console.log(rooms);
 
 // first suspect in the suspects array
-console.log(suspects[0]);
+// console.log(suspects[0]);
 
 // last room in the room array
-console.log(rooms[rooms.length - 1]);
+// console.log(rooms[rooms.length - 1]);
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
@@ -203,47 +203,67 @@ const randomSelector = array => {
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
-let mystery = {
+const mystery = {
   killer: '',
   room: '',
   weapon: ''
-}
-console.log(mystery)
+};
+// console.log(mystery)
+
+const killerCard = document.getElementById('killerCard');
+const killerName = document.getElementById('killerName');
+const killerImage = document.getElementById('killerImage');
+const weaponCard = document.getElementById('weaponCard');
+const weaponName = document.getElementById('weaponName');
+const roomCard = document.getElementById('roomCard');
+const roomName = document.getElementById('roomName');
+const revealedMystery = document.getElementById('mystery');
+const revealButton = document.getElementById('revealButton');
+const resetButton = document.getElementById('resetButton');
 
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
+  mystery.killer = randomSelector(suspects);
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
-  document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById('killerName').innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
-  document.getElementById('killerImage').src = mystery.killer.image
-}
+  killerCard.style.background = mystery.killer.color;
+  killerName.innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+  killerImage.src = mystery.killer.image;
+};
 
-document.getElementById('killerCard').onclick = pickKiller
+killerCard.onclick = pickKiller;
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
 const pickWeapon = () => {
-  mystery.weapon = randomSelector(weapons)
-  document.getElementById('weaponName').innerHTML = `${mystery.weapon.name}`
-}
+  mystery.weapon = randomSelector(weapons);
+  weaponName.innerHTML = mystery.weapon.name;
+};
 
-document.getElementById('weaponCard').onclick = pickWeapon
+weaponCard.onclick = pickWeapon;
 
 const pickRoom = () => {
-  mystery.room = randomSelector(rooms)
-  document.getElementById('roomName').innerHTML = `${mystery.room}`
-}
+  mystery.room = randomSelector(rooms);
+  roomName.innerHTML = mystery.room;
+};
 
-document.getElementById('roomCard').onclick = pickRoom
+roomCard.onclick = pickRoom;
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
 
 const revealMystery = () => {
-  document.getElementById('mystery').innerHTML = (`The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName}, in the ${mystery.room} with a ${mystery.weapon.name}.`)
-}
+  if (mystery.killer === '' || mystery.weapon === '' || mystery.room === '') {
+    revealedMystery.innerHTML = ('Make sure you have picked a killer, a weapon and a room!');
+  } else {
+    revealedMystery.innerHTML = (`The murder was committed by ${mystery.killer.firstName} ${mystery.killer.lastName} with the ${mystery.weapon.name} in the ${mystery.room}.`);
+  }
+};
 
-document.querySelector('button').onclick = revealMystery
+const resetMystery = () => {
+  location.reload();
+};
+
+revealButton.onclick = revealMystery;
+resetButton.onclick = resetMystery;
