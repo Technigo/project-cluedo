72c3f181c9fd23addee60dea161eab00e0a0d2d8
Emelie Svensson <emeliesv@live.se>
2020-09-16 09:17:15 +0200

    changed all id-names to kebab-case

--- code/clue.js
+++ code/clue.js
@@ -1,8 +1,6 @@
-// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
-
 const suspects = [
   {
-    name: 'Mr Green',
+    name: "Mr Green",
     firstName: "Jacob",
     lastName: "Green",
     color: "green",
@@ -12,7 +10,7 @@ const suspects = [
     occupation: "Entrepreneur",
   },
   {
-    name: 'Miss Scarlet',
+    name: "Miss Scarlet",
     firstName: "Cassandra",
     lastName: "Scarlet",
     color: "red",
@@ -22,7 +20,7 @@ const suspects = [
     occupation: "Writer",
   },
   {
-    name: 'Professor Plum',
+    name: "Professor Plum",
     firstName: "Victor",
     lastName: "Plum",
     color: "purple",
@@ -32,7 +30,7 @@ const suspects = [
     occupation: "Professor in detergents",
   },
   {
-    name: 'Mrs Peacock',
+    name: "Mrs Peacock",
     firstName: "Eleanor",
     lastName: "Peacock",
     color: "blue",
@@ -42,7 +40,7 @@ const suspects = [
     occupation: "Entrepreneur",
   },
   {
-    name: 'Colonel Mustard',
+    name: "Colonel Mustard",
     firstName: "Jack",
     lastName: "Mustard",
     color: "yellow",
@@ -52,7 +50,7 @@ const suspects = [
     occupation: "Colonel",
   },
   {
-    name: 'Mrs White',
+    name: "Mrs White",
     firstName: "Mrs",
     lastName: "White",
     color: "white",
@@ -116,133 +114,152 @@ const weapons = [
 const rooms = [
   {
     name: "Dining room",
-    description: "We enter the Dining room. A long, dark room lighted with candles. A big dining table and 12 chairs sits in the middle. Under the table we see the body, feet sticking out under the white linen cloth.",
-    image: "assets/diningroom.jpg"
+    description:
+      "We enter the Dining room. A long, dark room lighted with candles. A big dining table and 12 chairs sits in the middle. Under the table we see the body, feet sticking out under the white linen cloth.",
+    image: "assets/diningroom.jpg",
   },
   {
     name: "Conservatory",
-    description: "We enter the Conservatory. It's a big, glass conservatory filled with mediterrainian plants. On a bed of roses the victim rests.. Quite poetic..",
-    image: "assets/conservatory.jpg"
+    description:
+      "We enter the Conservatory. It's a big, glass conservatory filled with mediterrainian plants. On a bed of roses the victim rests.. Quite poetic..",
+    image: "assets/conservatory.jpg",
   },
   {
     name: "Theater",
-    description: "We enter the Theater. To be or not to be. Well, in this case the victim is no more.. The red velvet curtains drapering the stage is a perfect disguise for a body",
-    image: "assets/theater.jpg"
+    description:
+      "We enter the Theater. To be or not to be. Well, in this case the victim is no more.. The red velvet curtains drapering the stage is a perfect disguise for a body",
+    image: "assets/theater.jpg",
   },
   {
-    name: 'Kitchen',
-    description: "We enter the Kitchen. It's filled with sharp knifes and boiling pots. The body is found in the pantry, flour spilled everywhere. Such a mess..",
-    image: "assets/kitchen.jpg"
+    name: "Kitchen",
+    description:
+      "We enter the Kitchen. It's filled with sharp knifes and boiling pots. The body is found in the pantry, flour spilled everywhere. Such a mess..",
+    image: "assets/kitchen.jpg",
   },
   {
-    name: 'Study',
-    description: 'We enter the Study. Bookcases from the floor reaching the ceiling, but all of the books are on the floor. There sure was a scuffle going on here. The result: Body under the desk.',
-    image: "assets/study.jpg"
+    name: "Study",
+    description:
+      "We enter the Study. Bookcases from the floor reaching the ceiling, but all of the books are on the floor. There sure was a scuffle going on here. The result: Body under the desk.",
+    image: "assets/study.jpg",
   },
   {
-    name: 'Library',
-    description: 'We enter the Library. Large bookcases covers the wall. A ladder is resting against one bookcase, and in the space between the bookcase and ceiling the body is found. How did the killer get it up there?',
-    image: "assets/library.jpg"
+    name: "Library",
+    description:
+      "We enter the Library. Large bookcases covers the wall. A ladder is resting against one bookcase, and in the space between the bookcase and ceiling the body is found. How did the killer get it up there?",
+    image: "assets/library.jpg",
   },
   {
-    name: 'Billiard room',
-    description: "We enter the Billiard room. It's dark, woodpaneled room with a huge billiardtable in the middle. Of course, the body is found lying straight on the table. What a mess..",
-    image: "assets/billiard.jpg"
+    name: "Billiard room",
+    description:
+      "We enter the Billiard room. It's dark, woodpaneled room with a huge billiardtable in the middle. Of course, the body is found lying straight on the table. What a mess..",
+    image: "assets/billiard.jpg",
   },
   {
-    name: 'Lounge',
-    description: "We enter the Lounge. Velvet couches and a broken glasstable. Sherry is spilled on a dark, blue carpet. Wait a minute.. that's not sherry! A body is sticking out under one of the couches..",
-    image: "assets/lounge.jpg"
+    name: "Lounge",
+    description:
+      "We enter the Lounge. Velvet couches and a broken glasstable. Sherry is spilled on a dark, blue carpet. Wait a minute.. that's not sherry! A body is sticking out under one of the couches..",
+    image: "assets/lounge.jpg",
   },
   {
-    name: 'Ballroom',
-    description: "We enter the Ballroom. Once there was a ball filled with joy and music. Now there's only tragic, blood and death. The body is found in a corner of the room, the once so shiny wooden floor is now covered in blood.",
-    image: "assets/ballroom.jpg"
+    name: "Ballroom",
+    description:
+      "We enter the Ballroom. Once there was a ball filled with joy and music. Now there's only tragic, blood and death. The body is found in a corner of the room, the once so shiny wooden floor is now covered in blood.",
+    image: "assets/ballroom.jpg",
   },
   {
-    name: 'Hallway',
-    description: 'We enter the Hallway. Creeky floorboards and portraits glaring. The killer must have been very quiet and have known exactly which boards to avoid to not be discovered. Behind a big plant we find the body, the former lady of the house sneering from her portrait.',
-    image: "assets/hallway.jpg"
+    name: "Hallway",
+    description:
+      "We enter the Hallway. Creeky floorboards and portraits glaring. The killer must have been very quiet and have known exactly which boards to avoid to not be discovered. Behind a big plant we find the body, the former lady of the house sneering from her portrait.",
+    image: "assets/hallway.jpg",
   },
   {
-    name: 'Spa',
-    description: "We enter the Spa. Soft music playing, the smell of chlorine and moist air. If it wasn't for the body floating in the pool, it would be quite inviting.",
-    image: "assets/spa.jpg"
+    name: "Spa",
+    description:
+      "We enter the Spa. Soft music playing, the smell of chlorine and moist air. If it wasn't for the body floating in the pool, it would be quite inviting.",
+    image: "assets/spa.jpg",
   },
   {
-    name: 'Living Room',
-    description: "We enter the Living room. With the cracked tv's distorted sound in the background and whiskeybottle on the floor, there are plenty of evidence of a scuffle. The body was carefully placed in a chair in front of the tv.",
-    image: "assets/tvroom.jpg"
+    name: "Living Room",
+    description:
+      "We enter the Living room. With the cracked tv's distorted sound in the background and whiskeybottle on the floor, there are plenty of evidence of a scuffle. The body was carefully placed in a chair in front of the tv.",
+    image: "assets/tvroom.jpg",
   },
   {
-    name: 'Guest House',
-    description: 'We enter the guest house. It has a spartan decor with two small beds. This is far from inviting, black mold covering the corners of the ceiling. The body was found in the closet, maybe the murderer was in a rush?',
-    image: "assets/guesthouse.jpg"
+    name: "Guest House",
+    description:
+      "We enter the guest house. It has a spartan decor with two small beds. This is far from inviting, black mold covering the corners of the ceiling. The body was found in the closet, maybe the murderer was in a rush?",
+    image: "assets/guesthouse.jpg",
   },
   {
-    name: 'Patio',
-    description: 'This patio could really use a renovation. Loose floorboards everywhere. A perfect place to hide a body.',
-    image: "assets/patio.jpg"
+    name: "Patio",
+    description:
+      "This patio could really use a renovation. Loose floorboards everywhere. A perfect place to hide a body.",
+    image: "assets/patio.jpg",
   },
 ];
 
-// THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
-// YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
 const randomSelector = (array) => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
-// With a killer, a weapon and a room.
-// The values will be set later.
 const mystery = {
   killer: "",
   weapon: "",
-  room: ""
-}
+  room: "",
+};
 
-// This function will be invoked when you click on the killer card.
 const pickKiller = () => {
-  // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects);
 
-  
-  document.getElementById("killerCard").style.background = mystery.killer.color;
+  document.getElementById("killer-card").style.background = mystery.killer.color;
   document.getElementById(
-    "killerName"
+    "killer-name"
   ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
-  document.getElementById('killerOccupation').innerHTML = `${mystery.killer.occupation}`
-  document.getElementById('killerImage').classList.remove("hidden")
-  document.getElementById('killerImage').src = mystery.killer.image;
-  document.getElementById('killerDescription').innerHTML = `${mystery.killer.description}`;
+  document.getElementById(
+    "killer-occupation"
+  ).innerHTML = `${mystery.killer.occupation}`;
+  document.getElementById("killer-image").classList.remove("hidden");
+  document.getElementById("killer-image").src = mystery.killer.image;
+  document.getElementById(
+    "killer-description"
+  ).innerHTML = `${mystery.killer.description}`;
 };
 
 const pickWeapon = () => {
   mystery.weapon = randomSelector(weapons);
   console.log(mystery);
 
-  document.getElementById("weaponCard").style.background = mystery.weapon.color;
-  document.getElementById("weaponName").innerHTML = `${mystery.weapon.name}`;
+  document.getElementById("weapon-card").style.background = mystery.weapon.color;
+  document.getElementById("weapon-name").innerHTML = `${mystery.weapon.name}`;
   document.getElementById(
-    "weaponWeight"
+    "weapon-weight"
   ).innerHTML = `${mystery.weapon.weight} lbs`;
   document.getElementById(
-    "weaponDescription"
+    "weapon.description"
   ).innerHTML = `${mystery.weapon.description}`;
 };
 const pickRoom = () => {
   mystery.room = randomSelector(rooms);
-  document.getElementById("roomName").innerHTML = `${mystery.room.name}`;
-  document.getElementById('roomImage').classList.remove("hidden")
-  document.getElementById('roomImage').src = mystery.room.image;
-  };
-
-const revealMystery = () => {
-  document.getElementById('messageBox').classList.remove('hidden');
-  document.getElementById('instructions').classList.add('hidden');
-
-  document.getElementById('mystery').innerHTML = `The murder was commited by ${mystery.killer.name} in the ${mystery.room.name} with the ${mystery.weapon.name}. Scandalous!`;
-  document.getElementById("revealMessage").innerHTML = `In the darkest hour, while everyone else were busy doing other stuff ${mystery.killer.name} acted quickly.`
-  document.getElementById('roomDescription').innerHTML = `${mystery.room.description}`;
+  document.getElementById("room-name").innerHTML = `${mystery.room.name}`;
+  document.getElementById("room-image").classList.remove("hidden");
+  document.getElementById("room-image").src = mystery.room.image;
 };
 
+const revealMystery = () => {
+  document.getElementById("message-box").classList.remove("hidden");
+  document.getElementById("instructions").classList.add("hidden");
+
+  if (mystery.killer === "" || mystery.weapon === "" || mystery.room === "") {
+    document.getElementById("mystery").innerHTML = `Choose cards first!`
+  } else{
+  document.getElementById(
+    "mystery"
+  ).innerHTML = `The murder was commited by ${mystery.killer.name} in the ${mystery.room.name} with the ${mystery.weapon.name}. Scandalous!`;
+  document.getElementById(
+    "reveal-message"
+  ).innerHTML = `In the darkest hour, while everyone else were busy doing other stuff ${mystery.killer.name} acted quickly.`;
+  document.getElementById(
+    "room-description"
+  ).innerHTML = `${mystery.room.description}`;
+}
+};
