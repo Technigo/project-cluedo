8728894c71943b0a6122a7270a857e14200479cc
elisa blomberg <elisa.blomberg@outlook.com>
2020-09-17 10:11:09 +0200

    Step 2 done

--- code/clue.js
+++ code/clue.js
@@ -1,130 +1,124 @@
-// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
-
-const mrGreen = {
-  firstName: 'Jacob',
-  lastName: 'Green',
-  color: 'green',
-  description: 'He has a lot of connections',
-  age: 70,
-  image: 'assets/green.png',
-  occupation: 'Entrepreneur'
-};
-
-const professorPlum = {
-  firstName: 'Victor',
-  lastName: 'Plum',
-  color: 'purple',
-  description: 'A professor in history and criminal technology',
-  age: 62,
-  image: 'plum.png',
-  occupation: 'Detective'
-}
-const missScarlet = {
-  firstName: 'Cassandra',
-  lastName: 'Scarlet',
-  color: 'red',
-  description: 'A famous actor, been in many movies. Love antrophology',
-  age: 47,
-  image: 'scarlet.png',
-  occupation: 'Actor'
-}
-const mrsPeacock = {
-  firstName: 'Eleonor',
-  lastName: 'Peacock',
-  color: 'blue',
-  description: 'Degree in criminal history and writing. Love tea and cookies',
-  age: 59,
-  image: 'assets/green.png',
-  occupation: 'Author'
-}
-const colonelMustard = {
-  firstName: 'Jack',
-  lastName: 'Mustard',
-  color: 'yellow',
-  description: 'Passion of the military and hunting on his free time',
-  age: 76,
-  image: 'mustard.png',
-  occupation: 'Military Officer'
-}
-const mrsWhite = {
-  firstName: 'Jane',
-  lastName: 'White',
-  color: 'white',
-  description: 'A nurse that worked as a midwife all her life',
-  age: 74,
-  image: 'white.png',
-  occupation: 'Nurse'
-}
-
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
-
-const rope = {
-  name: 'rope',
-  weight: 10
-}
-const knife = {
-  name: 'knife',
-  weight: 1
-}
-const candlestick = {
-  name: 'candlestick',
-  weight: 0.1
-}
-const dumbbell = {
-  name: 'dumbbell',
-  weight: 5
-}
-const poison = {
-  name: 'poison',
-  weight: 0.2
-}
-const axe = {
-  name: 'axe',
-  weight: 6
-}
-const bat = {
-  name: 'bat',
-  weight: 1.5
-}
-const trophy = {
-  name: 'trophy',
-  weight: 4
-}
-const pistol = {
-  name: 'pistol',
-  weight: 2
-}
-
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
-/*
-const rooms 
-Dining Room 
-Conservatory 
-Kitchen 
-Study 
-Library 
-Billiard Room 
-Lounge 
-Ballroom 
-Hall 
-Spa 
-Living Room 
-Observatory 
-Theater 
-Guest House 
-Patio
-*/
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
-
 const suspects = [
-  mrGreen,
-  mrsWhite
-  // ...  and the rest
-]
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    color: 'green',
+    description: 'He has a lot of connections',
+    age: 70,
+    image: 'assets/green.png',
+    occupation: 'Entrepreneur'
+  },
+  {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    color: 'purple',
+    description: 'A professor in history and criminal technology',
+    age: 62,
+    image: 'plum.png',
+    occupation: 'Detective'
+  },
+  {
+    firstName: 'Cassandra',
+    lastName: 'Scarlet',
+    color: 'red',
+    description: 'A famous actor, been in many movies. Love antrophology',
+    age: 47,
+    image: 'scarlet.png',
+    occupation: 'Actor'
+  },
+  {
+    firstName: 'Eleonor',
+    lastName: 'Peacock',
+    color: 'blue',
+    description: 'Degree in criminal history and writing. Love tea and cookies',
+    age: 59,
+    image: 'assets/green.png',
+    occupation: 'Author'
+  },
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    color: 'yellow',
+    description: 'Passion of the military and hunting on his free time',
+    age: 76,
+    image: 'mustard.png',
+    occupation: 'Military Officer'
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'White',
+    color: 'white',
+    description: 'A nurse that worked as a midwife all her life',
+    age: 74,
+    image: 'white.png',
+    occupation: 'Nurse'
+  }
+];
+/*suspects.forEach((suspect) => {
+  console.log(`${suspect.color}`)
+}); */
+console.log(suspects[3]);
 
-const weapons = []
+const weapons = [
+  {
+    name: 'rope',
+    weight: 10
+  },
+  {
+    name: 'knife',
+    weight: 1
+  },
+  {
+    name: 'candlestick',
+    weight: 0.1
+  },
+  {
+    name: 'dumbbell',
+    weight: 5
+  },
+  {
+    name: 'poison',
+    weight: 0.2
+  },
+  {
+    name: 'axe',
+    weight: 6
+  },
+  {
+    name: 'bat',
+    weight: 1.5
+  },
+  {
+    name: 'trophy',
+    weight: 4
+  },
+  {
+    name: 'pistol',
+    weight: 2
+  }
+];
+weapons.forEach((weapon) => {
+  console.log(`${weapon.weight}`)
+});
 
-const rooms = []
+const rooms = [
+  'diningRoom', 
+  'conservatory', 
+  'kitchen', 
+  'study', 
+  'library', 
+  'billiardRoom', 
+  'lounge', 
+  'ballroom', 
+  'hall', 
+  'spa', 
+  'livingRoom', 
+  'observatory', 
+  'theatre', 
+  'guestHouse', 
+  'patio'
+];
+console.log(rooms[0]);
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
