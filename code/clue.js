1d0e5150c7d3604b0fb9eddbbcb325126dace80e
elisa blomberg <elisa.blomberg@outlook.com>
2020-09-17 22:18:33 +0200

    Googled a lot and searched for information, finally got some things to work as it should (oncklick, card will change, etc)

--- code/clue.js
+++ code/clue.js
@@ -1,105 +1,118 @@
+const mrGreen = {
+  firstName: 'Jacob',
+  lastName: 'Green',
+  color: 'green',
+  description: 'He has a lot of connections',
+  age: 70,
+  image: 'assets/green.png',
+  occupation: 'Entrepreneur'
+};
+
+const professorPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  color: 'purple',
+  description: 'A professor in history and criminal technology',
+  age: 62,
+  image: 'plum.png',
+  occupation: 'Detective'
+}
+const missScarlet = {
+  firstName: 'Cassandra',
+  lastName: 'Scarlet',
+  color: 'red',
+  description: 'A famous actor, been in many movies. Love antrophology',
+  age: 47,
+  image: 'scarlet.png',
+  occupation: 'Actor'
+}
+const mrsPeacock = {
+  firstName: 'Eleonor',
+  lastName: 'Peacock',
+  color: 'blue',
+  description: 'Degree in criminal history and writing. Love tea and cookies',
+  age: 59,
+  image: 'assets/green.png',
+  occupation: 'Author'
+}
+const colonelMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  color: 'yellow',
+  description: 'Passion of the military and hunting on his free time',
+  age: 76,
+  image: 'mustard.png',
+  occupation: 'Military Officer'
+}
+const mrsWhite = {
+  firstName: 'Jane',
+  lastName: 'White',
+  color: 'white',
+  description: 'A nurse that worked as a midwife all her life',
+  age: 74,
+  image: 'white.png',
+  occupation: 'Nurse'
+}
+// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
+
+const rope = {
+  name: 'rope',
+  weight: 10
+}
+const knife = {
+  name: 'knife',
+  weight: 1
+}
+const candlestick = {
+  name: 'candlestick',
+  weight: 0.1
+}
+const dumbbell = {
+  name: 'dumbbell',
+  weight: 5
+}
+const poison = {
+  name: 'poison',
+  weight: 0.2
+}
+const axe = {
+  name: 'axe',
+  weight: 6
+}
+const bat = {
+  name: 'bat',
+  weight: 1.5
+}
+const trophy = {
+  name: 'trophy',
+  weight: 4
+}
+const pistol = {
+  name: 'pistol',
+  weight: 2
+}
+
 const suspects = [
-  {
-    firstName: 'Jacob',
-    lastName: 'Green',
-    color: 'green',
-    description: 'He has a lot of connections',
-    age: 70,
-    image: 'assets/green.png',
-    occupation: 'Entrepreneur'
-  },
-  {
-    firstName: 'Victor',
-    lastName: 'Plum',
-    color: 'purple',
-    description: 'A professor in history and criminal technology',
-    age: 62,
-    image: 'plum.png',
-    occupation: 'Detective'
-  },
-  {
-    firstName: 'Cassandra',
-    lastName: 'Scarlet',
-    color: 'red',
-    description: 'A famous actor, been in many movies. Love antrophology',
-    age: 47,
-    image: 'scarlet.png',
-    occupation: 'Actor'
-  },
-  {
-    firstName: 'Eleonor',
-    lastName: 'Peacock',
-    color: 'blue',
-    description: 'Degree in criminal history and writing. Love tea and cookies',
-    age: 59,
-    image: 'assets/green.png',
-    occupation: 'Author'
-  },
-  {
-    firstName: 'Jack',
-    lastName: 'Mustard',
-    color: 'yellow',
-    description: 'Passion of the military and hunting on his free time',
-    age: 76,
-    image: 'mustard.png',
-    occupation: 'Military Officer'
-  },
-  {
-    firstName: 'Jane',
-    lastName: 'White',
-    color: 'white',
-    description: 'A nurse that worked as a midwife all her life',
-    age: 74,
-    image: 'white.png',
-    occupation: 'Nurse'
-  }
-];
-/*suspects.forEach((suspect) => {
-  console.log(`${suspect.color}`)
-}); */
-console.log(suspects[3]);
+  mrGreen,
+  professorPlum,
+  missScarlet,
+  mrsPeacock,
+  colonelMustard,
+  mrsWhite
+]
 
 const weapons = [
-  {
-    name: 'rope',
-    weight: 10
-  },
-  {
-    name: 'knife',
-    weight: 1
-  },
-  {
-    name: 'candlestick',
-    weight: 0.1
-  },
-  {
-    name: 'dumbbell',
-    weight: 5
-  },
-  {
-    name: 'poison',
-    weight: 0.2
-  },
-  {
-    name: 'axe',
-    weight: 6
-  },
-  {
-    name: 'bat',
-    weight: 1.5
-  },
-  {
-    name: 'trophy',
-    weight: 4
-  },
-  {
-    name: 'pistol',
-    weight: 2
-  }
-];
-weapons.forEach((weapon) => {
-  console.log(`${weapon.weight}`)
-});
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+]
+
 
 const rooms = [
   'diningRoom', 
@@ -117,8 +130,7 @@ const rooms = [
   'theatre', 
   'guestHouse', 
   'patio'
-];
-console.log(rooms[0]);
+]
 
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
@@ -126,21 +138,29 @@ const randomSelector = array => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
+
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
 
+const mystery = {
+killer: '',
+weapon: '',
+room: ''
+//killer: null, tex
+}
+
 // This function will be invoked when you click on the killer card.
-const pickKiller = () => {
+function pickKiller(mystery) {
+
   // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
+  mystery.killer = randomSelector(suspects);
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
-  document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById(
-    'killerName'
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+  document.getElementById('killerCard').style.background = mystery.killer.color;
+  document.getElementById('killerName').innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
 }
+document.getElementById('killerCard').addEventListener('click', pickKiller);
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
 
