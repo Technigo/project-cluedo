445c382472f3e840cd3d9bb9d70d1393fcfcc70a
Moa <63230455+mhammarback@users.noreply.github.com>
2020-09-17 09:58:03 +0200

    formated js

--- code/clue.js
+++ code/clue.js
@@ -123,7 +123,7 @@ const bat = {
   id: 'bat',
   name: 'bat',
   weight: 7,
-  color: 'brown'
+  color: 'black'
 }
 
 const trophy = {
@@ -138,7 +138,6 @@ const pistol = {
   name: 'pistol',
   weight: 8,
   color: 'black'
-
 }
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
@@ -212,44 +211,47 @@ const pickKiller = () => {
   // This will randomly select a killer from the suspects. And add that to the mystery object.
   mystery.killer = randomSelector(suspects)
 
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
-  document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById('killerName').innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
-  document.getElementById('killerOccupation').innerHTML = `${mystery.killer.occupation}`;
+  const killerName = document.getElementById('killerName')
+  const killerOccupation = document.getElementById('killerOccupation')
+  const killerCard = document.getElementById('killerCard')
   const killerImage = document.getElementById('killerImage');
+
+  killerCard.style.background = mystery.killer.color
+  killerName.innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+  killerOccupation.innerHTML = `${mystery.killer.occupation}`;
   killerImage.src = mystery.killer.image;
 
   // shuffel favorite weapon function.
   const shuffleFavouriteWeapon = () => {
-    mystery.favouriteWeapon = randomSelector(weapons);
-    document.getElementById('favouriteWeapon').innerHTML = `Favourite weapon: ${mystery.favouriteWeapon.name}`;
+  mystery.favouriteWeapon = randomSelector(weapons);
+  document.getElementById('favouriteWeapon').innerHTML = `Favourite weapon: ${mystery.favouriteWeapon.name}`;
   }
   shuffleFavouriteWeapon();
 };
 
 
-// CREATE FUNCTION pickWeapon 
+// pickWeapon function
 
 const pickWeapon = () => {
   // This will randomly select a weapon. And add that to the mystery object.
   mystery.weapons = randomSelector(weapons)
 
-  document.getElementById('weaponCard').style.background = mystery.weapons.color
-  document.getElementById(
-    'weaponName'
-  ).innerHTML = `${mystery.weapons.name}`
+  const weaponCard = document.getElementById('weaponCard')
+  const weaponName = document.getElementById('weaponName')
+
+  weaponCard.style.background = mystery.weapons.color
+  weaponName.innerHTML = `${mystery.weapons.name}`
 }
 
 
 // CREATE FUNCTION pickRoom 
 
 const pickRoom = () => {
-  // This will randomly select a weapon. And add that to the mystery object.
+  // This will randomly select a room. And add that to the mystery object.
   mystery.rooms = randomSelector(rooms)
 
-  document.getElementById(
-    'roomName'
-  ).innerHTML = `${mystery.rooms}`
+  const roomName = document.getElementById('roomName')
+  roomName.innerHTML = `${mystery.rooms}`
 }
 
 
@@ -259,9 +261,8 @@ document.getElementById('weaponCard').onclick = pickWeapon;
 document.getElementById('roomCard').onclick = pickRoom;
 
 
+// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. 
 
-// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
-// 'The murder was committed by Jacob Green, in the living room with a rope.'
 
 const revealMystery = () => {
   document.getElementById(
