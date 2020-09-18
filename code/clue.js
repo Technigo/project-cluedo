58212ac520a9816f0ad130c4d32514973ad48aa2
Nadia <63302901+Bluepoisson@users.noreply.github.com>
2020-09-18 07:12:25 +0200

    Blue level
    All steps done.

--- code/clue.js
+++ code/clue.js
@@ -160,6 +160,11 @@ const randomSelector = (array) => {
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY.
 // With a killer, a weapon and a room.
 // The values will be set later.
+const mystery = {
+  killer: "",
+  weapons: "",
+  room: "",
+};
 
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
@@ -171,9 +176,32 @@ const pickKiller = () => {
   document.getElementById(
     "killerName"
   ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+  document.getElementById("killerage").innerHTML = `${mystery.killer.age}`;
+  document.getElementById("killerCard").onclick = pickKiller;
 };
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+const pickWeapon = () => {
+  mystery.weapon = randomSelector(weapons);
+
+  document.getElementById(
+    "weaponName"
+  ).innerHTML = `${mystery.weapon.name} ${mystery.weapon.weight}`;
+  document.getElementById("weaponCard").onclick = pickWeapon;
+};
+
+const pickRoom = () => {
+  mystery.room = randomSelector(rooms);
+
+  document.getElementById("roomName").innerHTML = `${mystery.room}`;
+  document.getElementById("roomCard").onclick = pickRoom;
+};
 
 // STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
 // 'The murder was committed by Jacob Green, in the living room with a rope.'
+const revealMystery = () => {
+  document.getElementById(
+    "mystery"
+  ).innerHTML = `The murder was comitted by ${mystery.killer.firstName} in the ${mystery.room} with a ${mystery.weapon.name}`;
+  document.getElementById("mysteryBtn").onclick = revealMystery;
+};
