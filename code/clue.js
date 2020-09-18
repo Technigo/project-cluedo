ac410fc5faa6e8613e8ed5c54efea7de465bc0bc
Lili <lilihorvath@github.com>
2020-09-18 22:52:35 +0200

    First commit by BLUE

--- code/clue.js
+++ code/clue.js
@@ -1,59 +1,236 @@
-// STEP 1 - CREATE OBJECTS FOR ALL THE SUSPECTS, SOMETHING LIKE THIS:
+const Marcel = {
+  firstName: 'Marcel',
+  lastName: 'the Monkey',
+  description: 'He was born to steal',
+  age: 3,
+  image: 'assets/marcel.png',
+  occupation: 'Monkey',
+  favouriteFood: 'Banana'
+};
 
-const mrGreen = {
-  firstName: 'Jacob',
+const Ross = {
+  firstName: 'Ross',
+  lastName: 'Geller',
+  description: 'A palaeontologist working at the Museum of Natural History and in love with Rachel.',
+  age: 27,
+  image: 'assets/ross.png',
+  occupation: 'Palaeontologist',
+  favouriteFood: 'Muffin'
+};
+
+const Chandler = {
+  firstName: 'Chandler',
+  lastName: 'Muriel Bing',
+  description: 'Making jokes whe he feels himself uncomfortable and loves Monica.',
+  age: 27,
+  image: 'assets/chandler.png',
+  occupation: 'Transponster',
+  favouriteFood: 'Pizza'
+};
+
+const Monica = {
+  firstName: 'Monica',
+  lastName: 'Geller',
+  description: 'Monica was overweight as a child.',
+  age: 26,
+  image: 'assets/monica.png',
+  occupation: 'Chef',
+  favouriteFood: 'Kiwi-lime pie'
+};
+
+const Rachel = {
+  firstName: 'Rachel',
   lastName: 'Green',
-  color: 'green',
-  description: 'He has a lot of connections',
-  age: 45,
-  image: 'assets/green.png',
-  occupation: 'Entrepreneur'
+  description: 'A fashion enthusiast and Monica Gellers best friend from childhood.',
+  age: 25,
+  image: 'assets/rachel.png',
+  occupation: 'Waitress',
+  favouriteFood: 'Trifle'
+};
+
+const Phoebe = {
+  firstName: 'Phoebe',
+  lastName: 'Buffay',
+  description: 'She writes and sings her own strange songs, accompanying herself on the guitar.',
+  age: 27,
+  image: 'assets/phoebe.png',
+  occupation: 'A self-taught musician.',
+  favouriteFood: 'Nothing which had a name'
+};
+
+const Hamburger = {
+  name: 'Hamburger',
+  taste: 8,
+  image: 'assets/hamburger.jpg'
 }
 
-// CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
-
-const rope = {
-  name: 'rope',
-  weight: 10
+const RachelTrifle = {
+  name: "Rachel's Trifle",
+  taste: 2,
+  image: 'assets/trifle.jpg'
 }
 
-// THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
+const Turkey = {
+  name: 'Turkey',
+  taste: 7,
+  image: 'assets/turkey.jpeg'
+}
 
-// NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
+const RedBagel = {
+  name: 'Red Bagel',
+  taste: 6,
+  image: 'assets/bagel.jpg'
+}
 
-const suspects = [
-  mrGreen,
-  mrsWhite
-  // ...  and the rest
+const Pizza = {
+  name: 'Pizza',
+  taste: 7,
+  image: 'assets/pizza.png'
+}
+
+const Muffin = {
+  name: 'Muffin',
+  taste: 6,
+  image: 'assets/muffin.jpg'
+}
+
+const KiwiPie = {
+  name: 'Kiwi Pie',
+  taste: 9,
+  image: 'assets/pie.jpg'
+}
+
+const Sandwich = {
+  name: 'Sandwich',
+  taste: 5,
+  image: 'assets/sandwich.jpg'
+}
+
+const MonicaDiningRoom = {
+  name: "Monica's Dining Room",
+  image: 'assets/mdr.jpg'
+}
+
+const RossApartment = {
+  name: "Ross' Apartment",
+  image: 'assets/ra.jpg'
+}
+
+const ChandlerDiningRoom = {
+  name: "Chandler's Dining Room",
+  image: 'assets/cdr.jpg'
+}
+
+const RachelRoom = {
+  name: "Rachel's Room",
+  image: 'assets/rbr.jpg'
+}
+
+const MonicaRoom = {
+  name: "Monica's Room",
+  image: 'assets/mbr.png'
+}
+
+const MonicaLivingRoom = {
+  name: "Monica's Living room",
+  image: 'assets/mlr.jpg'
+}
+
+const CentralPerk = {
+  name: "Central Perk",
+  image: 'assets/cp.jpg'
+}
+
+const Balcony = {
+  name: "the Balcony",
+  image: 'assets/balcony.jpg'
+}
+
+const Hall = {
+  name: "the Hall",
+  image: 'assets/stairwell.jpg'
+}
+
+const ChandlerLivingRoom = {
+  name: "Chandler's Living Room",
+  image: 'assets/clr.jpg'
+}
+
+const friend = [
+  Ross,
+  Chandler,
+  Monica,
+  Rachel,
+  Phoebe,
+  Marcel
+];
+
+const foods = [
+  Hamburger,
+  RachelTrifle,
+  Turkey,
+  RedBagel,
+  Pizza,
+  Muffin,
+  KiwiPie,
+  Sandwich,
+];
+
+const rooms = [
+  MonicaDiningRoom,
+  RossApartment,
+  ChandlerDiningRoom, 
+  RachelRoom, 
+  MonicaRoom, 
+  MonicaLivingRoom, 
+  CentralPerk, 
+  Balcony, 
+  Hall, 
+  ChandlerLivingRoom
 ]
 
-const weapons = []
-
-const rooms = []
-
-// THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
-// YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
 const randomSelector = array => {
   return array[Math.floor(Math.random() * array.length)]
 }
 
-// CREATE AN OBJECT THAT KEEPS THE MYSTERY.
-// With a killer, a weapon and a room.
-// The values will be set later.
-
-// This function will be invoked when you click on the killer card.
-const pickKiller = () => {
-  // This will randomly select a killer from the suspects. And add that to the mystery object.
-  mystery.killer = randomSelector(suspects)
-
-  // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
-  document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById(
-    'killerName'
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`
+const mystery = {
+  friend: undefined,
+  foods: undefined,
+  rooms: undefined
 }
 
-// CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
+const pickFriends = () => {
+  mystery.friends = randomSelector(friend)
+  document.getElementById('friendsName').innerHTML = `${mystery.friends.firstName} ${mystery.friends.lastName}`;
+  document.getElementById('friendsAge').innerHTML = `${mystery.friends.age}`;
+  document.getElementById('friendsOccupation').innerHTML = mystery.friends.occupation;
+  document.getElementById('friendsDescription').innerHTML = mystery.friends.description;
+  document.getElementById('friendsImage').src = mystery.friends.image
+}
 
-// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
-// 'The murder was committed by Jacob Green, in the living room with a rope.'
+document.getElementById('friendsCard').onclick = pickFriends
+
+const pickFoods = () => {
+  mystery.foods = randomSelector(foods)
+  document.getElementById('foodsName').innerHTML = mystery.foods.name;
+  document.getElementById('foodsImage').src = mystery.foods.image
+}
+
+document.getElementById('foodCard').onclick = pickFoods
+
+const pickRooms = () => {
+  mystery.rooms = randomSelector(rooms)
+  document.getElementById('roomsName').innerHTML = `${mystery.rooms.name}`
+  document.getElementById('roomsImage').src = mystery.rooms.image
+}
+
+document.getElementById('roomsCard').onclick = pickRooms
+
+const revealMystery = () => {
+  if (mystery.friends === undefined || mystery.foods === undefined || mystery.rooms === undefined ){
+    document.getElementById('mystery').innerHTML = "You must find Joey's food!"
+  } else {
+    document.getElementById('mystery').innerHTML = `The ONE who ate Joey's ${mystery.foods.name} was ${mystery.friends.firstName} ${mystery.friends.lastName}, in ${mystery.rooms.name}.`
+  }
+}
+document.getElementById('cookButton').onclick = revealMystery
