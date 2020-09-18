f8751023ab4f1939f87b6c0352dfbdba903b4790
Emelie Svensson <emeliesv@live.se>
2020-09-18 16:08:01 +0200

    changed pictures

--- code/clue.js
+++ code/clue.js
@@ -139,85 +139,91 @@ const rooms = [
     name: "Dining room",
     description:
       "We enter the Dining room. A long, dark room lighted with candles. A big dining table and 12 chairs sits in the middle. Under the table we see the body, feet sticking out under the white linen cloth.",
-    image: "assets/diningroom.jpg",
+    image: "assets/diningroom.png",
   },
   {
     name: "Conservatory",
     description:
       "We enter the Conservatory. It's a big, glass conservatory filled with mediterrainian plants. On a bed of roses the victim rests.. Quite poetic..",
-    image: "assets/conservatory.jpg",
+    image: "assets/conservatory.png",
   },
   {
     name: "Theater",
     description:
       "We enter the Theater. To be or not to be. Well, in this case the victim is no more.. The red velvet curtains drapering the stage is a perfect disguise for a body",
-    image: "assets/theater.jpg",
+    image: "assets/theater.png",
   },
   {
     name: "Kitchen",
     description:
       "We enter the Kitchen. It's filled with sharp knifes and boiling pots. The body is found in the pantry, flour spilled everywhere. Such a mess..",
-    image: "assets/kitchen.jpg",
+    image: "assets/kitchen.png",
   },
   {
     name: "Study",
     description:
       "We enter the Study. Bookcases from the floor reaching the ceiling, but all of the books are on the floor. There sure was a scuffle going on here. The result: Body under the desk.",
-    image: "assets/study.jpg",
+    image: "assets/study.png",
   },
   {
     name: "Library",
     description:
       "We enter the Library. Large bookcases covers the wall. A ladder is resting against one bookcase, and in the space between the bookcase and ceiling the body is found. How did the killer get it up there?",
-    image: "assets/library.jpg",
+    image: "assets/library.png",
   },
   {
     name: "Billiard room",
     description:
       "We enter the Billiard room. It's dark, woodpaneled room with a huge billiardtable in the middle. Of course, the body is found lying straight on the table. What a mess..",
-    image: "assets/billiard.jpg",
+    image: "assets/billiard.png",
   },
   {
     name: "Lounge",
     description:
       "We enter the Lounge. Velvet couches and a broken glasstable. Sherry is spilled on a dark, blue carpet. Wait a minute.. that's not sherry! A body is sticking out under one of the couches..",
-    image: "assets/lounge.jpg",
+    image: "assets/lounge.png",
   },
   {
     name: "Ballroom",
     description:
       "We enter the Ballroom. Once there was a ball filled with joy and music. Now there's only tragic, blood and death. The body is found in a corner of the room, the once so shiny wooden floor is now covered in blood.",
-    image: "assets/ballroom.jpg",
+    image: "assets/ballroom.png",
   },
   {
     name: "Hallway",
     description:
       "We enter the Hallway. Creeky floorboards and portraits glaring. The killer must have been very quiet and have known exactly which boards to avoid to not be discovered. Behind a big plant we find the body, the former lady of the house sneering from her portrait.",
-    image: "assets/hallway.jpg",
+    image: "assets/hall.png",
   },
   {
     name: "Spa",
     description:
       "We enter the Spa. Soft music playing, the smell of chlorine and moist air. If it wasn't for the body floating in the pool, it would be quite inviting.",
-    image: "assets/spa.jpg",
+    image: "assets/spa.png",
   },
   {
     name: "Living Room",
     description:
       "We enter the Living room. With the cracked tv's distorted sound in the background and whiskeybottle on the floor, there are plenty of evidence of a scuffle. The body was carefully placed in a chair in front of the tv.",
-    image: "assets/tvroom.jpg",
+    image: "assets/livingroom.png",
   },
   {
     name: "Guest House",
     description:
       "We enter the guest house. It has a spartan decor with two small beds. This is far from inviting, black mold covering the corners of the ceiling. The body was found in the closet, maybe the murderer was in a rush?",
-    image: "assets/guesthouse.jpg",
+    image: "assets/guesthouse.png",
   },
   {
     name: "Patio",
     description:
       "This patio could really use a renovation. Loose floorboards everywhere. A perfect place to hide a body.",
-    image: "assets/patio.jpg",
+    image: "assets/patio.png",
+  },
+  {
+    name: "Observatory",
+    description:
+      "One would love to gaze at the stars here. Trinkets and books about space everywhere. Unfortunately also bloodspladder everywhere..",
+    image: "assets/observatory.png",
   },
 ];
 
@@ -267,6 +273,9 @@ const pickWeapon = () => {
   console.log(mystery);
 
   document.getElementById("weapon-card").style.background =
+    mystery.killer.color;
+
+  document.getElementById("weapon-card").style.background =
     mystery.weapon.color;
   document.getElementById("weapon-name").innerHTML = `${mystery.weapon.name}`;
   document.getElementById("weapon-image").classList.remove("hidden");
@@ -281,6 +290,8 @@ const pickWeapon = () => {
 
 const pickRoom = () => {
   mystery.room = randomSelector(rooms);
+  document.getElementById("room-card").style.background =
+    mystery.killer.color;
   document.getElementById("room-name").innerHTML = `${mystery.room.name}`;
   document.getElementById("room-image").classList.remove("hidden");
   document.getElementById("room-image").src = mystery.room.image;
