2cebe6206bfc8f58844a88729865fdfa1955e165
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-20 01:32:33 +0200

    Added pictures to all the cards. Tweaked the styling a bit. Tried to get the random weapon-function to work and the play-again-function. They still do not work. Cleaned the code.

--- code/clue.js
+++ code/clue.js
@@ -34,13 +34,13 @@ const missScarlet = {
   age: 41,
   image: 'assets/scarlet.png',
   occupation: 'Superhero',
-  favorite: 'dumbBell'
+  favorite: 'dumbbell'
 };
 
 const mrsPeacock = {
   firstName: 'Elanor',
   lastName: 'Peacock',
-  color: '#04b7d3',
+  color: '#156172',
   description: 'A real socialite.',
   age: 52,
   image: 'assets/peacock.png',
@@ -66,8 +66,8 @@ const mrsWhite = {
   description: 'She is quite strict and knows how to get things done.',
   age: 68,
   image: 'assets/white.png',
-  occupation: 'Retired project manager',
-  favorite: 'candleStick'
+  occupation: 'Retired',
+  favorite: 'candle stick'
 };
 
 //CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
@@ -77,7 +77,8 @@ const rope = {
   weight: 10,
   flexibility: 3,
   color: '#7f4d33',
-  id: "rope"
+  id: "rope",
+  image: 'assets/ropes1.jpg'
 };
 
 const knife = {
@@ -85,7 +86,8 @@ const knife = {
   weight: 1,
   flexibility: 9,
   color: '#2f6a49',
-  id: 'knife'
+  id: 'knife',
+  image: 'assets/knife.jpg'
 };
 
 const candleStick = {
@@ -93,7 +95,8 @@ const candleStick = {
   weight: 2,
   flexibility: 7,
   color: '#ddafd9',
-  id: 'candleStick'
+  id: 'candleStick',
+  image: 'assets/candlestick.jpg'
 };
 
 const dumbBell = {
@@ -101,15 +104,17 @@ const dumbBell = {
   weight: 20,
   flexibility: 6,
   color: '#9b2c15',
-  id: 'dumbBell'
+  id: 'dumbBell',
+  image: 'assets/dumbbell.jpg'
 };
 
 const poison = {
   name: 'poison',
   weight: 0,
   flexibility: 10,
-  color: '#04b7d3',
-  id: 'poison'
+  color: '#156172',
+  id: 'poison',
+  image: 'assets/poison.jpg'
 };
 
 const axe = {
@@ -117,7 +122,8 @@ const axe = {
   weight: 12,
   flexibility: 3,
   color: '#7b497f',
-  id: 'axe'
+  id: 'axe',
+  image: 'assets/axe.jpg'
 };
 
 const bat = {
@@ -125,7 +131,8 @@ const bat = {
   weight: 3,
   flexibility: 7,
   color: '#abb782',
-  id: 'bat'
+  id: 'bat',
+  image: 'assets/bat.jpg'
 };
 
 const trophy = {
@@ -133,7 +140,8 @@ const trophy = {
   weight: 5,
   flexibility: 3,
   color: '#fce853',
-  id: 'trophy'
+  id: 'trophy',
+  image: 'assets/trophy1.jpg'
 };
 
 const pistol = {
@@ -141,44 +149,85 @@ const pistol = {
   weight: 1,
   flexibility: 9,
   color: '#d1a93c',
-  id: 'pistol'
+  id: 'pistol',
+  image: 'assets/pistol.jpg'
 };
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
-const diningRoom = "Dining Room";
+const diningRoom = {
+  name: "Dining Room",
+  image: 'assets/dining.jpg'
+};
 
-const conservatory = "Conservatory";
+const conservatory = {
+  name: "Conservatory",
+  image: 'assets/conservatory.jpg'
+};
 
-const kitchen = "Kitchen";
+const kitchen = {
+  name: "Kitchen",
+  image: 'assets/kitchen.jpg'
+};
 
-const study = "Study";
+const study = {
+  name: "Study",
+  image: 'assets/study.jpg'
+};
 
-const library = "Library";
+const library = {
+  name: "Library",
+  image: 'assets/library.jpg'
+};
 
-const billiardRoom = "Billiard Room";
+const billiardRoom = {
+  name: "Billiard Room",
+  image: 'assets/billiard.jpg'
+};
 
-const lounge = "Lounge";
+const lounge = {
+  name: "Lounge",
+  image: 'assets/lounge.jpg'
+};
 
-const ballRoom = "Ball Room";
+const ballRoom = {
+  name: "Ball Room",
+  image: 'assets/ballroom.jpg'
+};
 
-const hall = "Hall";
+const hall = {
+  name: "Hall",
+  image: 'assets/hall.jpg'
+};
 
-const spa = "Spa";
+const spa = {
+  name: "Spa",
+  image: 'assets/spa.jpg'
+};
 
-const livingRoom = "Living Room";
+const livingRoom = {
+  name: "Living Room",
+  image: 'assets/livingroom.jpg'
+};
 
-const observatory = "Observatory";
+const observatory = {
+  name: "Observatory",
+  image: 'assets/observatory.jpg'
+};
 
-const theater = "Theater";
+const theater = {
+  name: "Theater",
+  image: 'assets/theater.jpg'
+};
 
-const guestHouse = "Guest House";
+const guestHouse = {
+  name: "Guest House",
+  image: 'assets/guesthouse.jpg'
+};
 
-const patio = "Patio";
-
-//CONSOLE LOG TO TEST:
-console.log(mrsWhite.description);
-console.log(candleStick.flexibility);
-console.log(mrsWhite.favorite); //returns undefined
+const patio = {
+  name: "Patio",
+  image: 'assets/patio.jpg'
+};
 
 // STEP 2. NOW GROUP ALL SUSPECTS, WEAPONS AND ROOMS IN ARRAYS LIKE THIS:
 const suspects = [
@@ -220,18 +269,11 @@ const rooms = [
   patio
 ];
 
-//console log the first suspect in the suspects array:
-console.log(suspects[0]);
-//console log the last room in the rooms array:
-console.log(rooms[14]); //How to do it if I dont know how many elements I have in my string?
-console.log(rooms.length); //writes 15 in console
-
 // THIS FUNCTION WILL RANDOMLY SELECT ONE ITEM FROM THE ARRAY THAT YOU PASS IN TO THE FUNCTION.
 // YOU DON'T NEED TO CHANGE THIS, JUST TRY TO UNDERSTAND IT. AND HOW TO USE IT.
 const randomSelector = array => { //I don't understand this
   return array[Math.floor(Math.random() * array.length)] //What does array.length do here?
 }
-//console.log(randomSelector(rooms)); //prints random room/suspect/weapon
 
 // CREATE AN OBJECT THAT KEEPS THE MYSTERY. With a killer, a weapon and a room. The values will be set later.
 const mystery = {
@@ -240,32 +282,38 @@ const mystery = {
   room: "",
 };
 
-//Function to shuffle the favourite weapons of the suspects in a randomized way. But how do I use it?
+//Function to shuffle the favourite weapons of the suspects in a randomized way.
 const shuffleWeapons = () => {
   suspects.forEach(suspect => {
-    suspect.favorite = randomSelector(weapons).id
-    console.log(suspect.favorite);
-    //suspect.favorite = weapons[Math.floor(Math.random() * 9)];
-    //console.log(suspect.favorite);
+    suspect.favorite = randomSelector(weapons).name
   })
 }
 
-
+//Function to increase probability of favourite weapon to be assigned to killer:
+//THIS FUNCTION DOESN'T WORK???
+const increaseChanceOfFavorite = () => {
+  const favWeapon = weapons.find(weapon => weapon.id === mystery.killer.favorite);
+  const favWeaponArray = new Array(7).fill(favWeapon);
+  console.log(favWeaponArray)
+  const calculatedWeapon = [...weapons, ...favWeaponArray];
+  return calculatedWeapon;
+};
 
 // This function will be invoked when you click on the killer card.
 const pickKiller = () => {
-  //randomly select a killer from the suspects. And add that to the mystery object.
   if (!killerIsPicked) {
     document.getElementById('killerLoader').style.visibility='visible' 
     setTimeout(function () {
       document.getElementById('killerLoader').style.visibility='hidden'
+      //excecutes shuffleWeapons-function above and makes favorite weapon random:
       shuffleWeapons();
+      //randomly select a killer from the suspects. And add that to the mystery object.
       mystery.killer = randomSelector(suspects); //mystery.killer selects the property 'killer' in mystery object and sets this equal to a randomly selected suspect. 
       document.getElementById('killerCard').style.background = mystery.killer.color
       document.getElementById('killerName').innerHTML = `Name: ${mystery.killer.firstName} ${mystery.killer.lastName}`
       document.getElementById('killerAge').innerHTML = `Age: ${mystery.killer.age}`
       document.getElementById('killerOccupation').innerHTML = `Occupation: ${mystery.killer.occupation}`
-      document.getElementById('killerWeapon').innerHTML = `Weapon of choice: ${mystery.killer.favorite}`
+      document.getElementById('killerWeapon').innerHTML = `Favorite weapon: ${mystery.killer.favorite}`
       document.getElementById('killerDescription').innerHTML = `Description: ${mystery.killer.description}`
       document.getElementById('killerImage').src = `${mystery.killer.image}`
       killerIsPicked = true;
@@ -273,13 +321,11 @@ const pickKiller = () => {
   }
 };
 //Added eventlistener below instead of "onclick" in HTML. Don't know which way is the best way to do it. This only works if the eventlistener is outside of the function. First param: type of event. Second param: the function to call on event.
-// document.getElementById('killerCard').addEventListener('click', pickKiller);
-//pickKiller();
-//console.log(mystery.killer); //Why does this return undefined in the console, but still it changes the first card in the browser and shows all the info about random killer above!?
-//console.log(`pickKiller.mystery.killer : ${pickKiller.mystery.killer}`); //Trying what Van did at the lecture, @about 18 min in, but doeasn't work?!?
+document.getElementById('killerCard').addEventListener('click', pickKiller);
+
 
 // CREATE FUNCTIONS pickWeapon and pickRoom in a similar way.
-//If pickKiller function is excecuted/card is clicked then do this function pickWeapon:
+//If pickKiller function is excecuted/card is clicked then excecute this function:
 const pickWeapon = () => {
   if (killerIsPicked && !weaponIsPicked) {
     document.getElementById('weaponLoader').style.visibility='visible'
@@ -290,14 +336,14 @@ const pickWeapon = () => {
   document.getElementById('weaponName').innerHTML = `${mystery.weapon.name}`
   document.getElementById('weaponWeight').innerHTML = `Weight: ${mystery.weapon.weight}`
   document.getElementById('weaponFlexibility').innerHTML = `Flexibility: ${mystery.weapon.flexibility}`
+  document.getElementById('weaponImage').src = `${mystery.weapon.image}`
   weaponIsPicked = true
     }, 2500)
   }
-  //Add image here for weapon-card later
 };
 document.getElementById('weaponCard').addEventListener('click', pickWeapon);
 
-//If pickKiller AND pickWeapon function is excecuted/cards are clicked then do this function on the pickroom:
+//If pickKiller AND pickWeapon function is excecuted/cards are clicked then excecute this function on the pickroom:
 const pickRoom = () => {
   if (killerIsPicked && weaponIsPicked) {
     document.getElementById('roomLoader').style.visibility='visible'
@@ -305,58 +351,36 @@ const pickRoom = () => {
     setTimeout(function () {
       document.getElementById('roomLoader').style.visibility='hidden'
       mystery.room = randomSelector(rooms);//Randomly select a room from rooms and add it to the mystery object
-      document.getElementById('roomName').innerHTML = `${mystery.room}`
+      document.getElementById('roomName').innerHTML = `${mystery.room.name}`
+      document.getElementById('roomImage').src = `${mystery.room.image}`
       roomIsPicked = true
     }, 2500)
   }
-  //Add image here for room-card later
 };
 document.getElementById('roomCard').addEventListener('click', pickRoom); 
 
-
-//Typ göra en ny funktion pageReload där jag gör ett if-statement som säger att om revealbutton har innertext play again så onclick så ska page reload ske. Annars...inte?
-
-
-
-// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. It should show something like:
-// 'The murder was committed by Jacob Green, in the living room with a rope.'
+// STEP 4 - CREATE A FUNCTION revealMystery that will be invoked when you click that button. 
 const revealMystery = (mystery) => {
-  //Invoke when button in html is clicked and show elements from mystery object in text.
-  //Put also some kind of if-statement(?) here that conditions this to reveal ONLY if the three cards above has been clicked.
-document.getElementById('mystery').innerHTML = `The murder was committed by ${mystery.killer.firstName} in the ${mystery.room} with ${mystery.weapon.name}` 
-document.getElementById('revealButton').innerHTML = `PLAY AGAIN?` //This works to change the text on button. But how do I get the page to reload when clicked again?
-//Can I put the function pageReload in this function? No it didn't work.
-//pageReload(); This makes the page reload once just after I clicked the Roomcard. Är detta för att villkoret i if-satsen i pageReload är uppfyllt?
+  //Invoke when button is clicked and show elements from mystery object in text:
+document.getElementById('mystery').innerHTML = `The murder was committed by ${mystery.killer.firstName} in the ${mystery.room.name} with ${mystery.weapon.name}` 
+document.getElementById('revealButton').innerHTML = `PLAY AGAIN?` //change the text on button.
 }
-// document.getElementById('revealButton').addEventListener('click', revealMystery(mystery)); 
 
-//THIS DOESN'T WORK EITHER:
- const playAgain = () => {
-   if (document.getElementById('revealButton').innerHtml === 'PLAY AGAIN?') {
-//      location.reload();
-        //revealButton.onclick === pageReload; 
-        revealButton.onclick === location.reload();
-    } 
-  };
-playAgain();
+//BELOW I TRIED TO MAKE A FUNCTION SO THAT THE PAGE RELOADS WHEN 'PLAY AGAIN'-BUTTON IS CLICKED. IT DIDN'T WORK. I wanted to leave this here just to show the code-reviewer what I tried and maybe get pointers on how to achieve this.
+
+//THIS DOESN'T WORK:
+//  const playAgain = () => {
+//    if (document.getElementById('revealButton').innerHtml === 'PLAY AGAIN?') {
+//         //location.reload();
+//         revealButton().onclick === pageReload; 
+//         //revealButton().onclick === location.reload();
+//     } 
+//   };
 //document.getElementById('revealButton').addEventListener('click', playAgain);
-//console.log(playAgain);
 
-// const buttonOnClick = () => {
-//  console.log('hej');
-//   if (document.getElementById('revealButton').innerHtml === 'REVEAL THE CRIME') {
-//     console.log('if-statement');
-//     revealMystery(mystery);
-//   } else if (document.getElementById('revealButton').innerHtml === `PLAY AGAIN?`) {
-//     location.reload();
-//   }
-// }
-// document.getElementById('revealButton').addEventListener('click', (buttonOnClick));
-//console.log(revealMystery(mystery)); If I put this console.log here the page reloads repeatedly. Why?
-
-//Denna funkar inte i alla fall härifrån:
+//THIS DOESN'T WORK:
 //const pageReload = () => {
-  //revealButton.onclick = pageReload; //Provar sätta den här.
+  //revealButton.onclick = pageReload; //Trying to put this here.
   //document.getElementById('revealButton').onclick.innerHTML = `PLAY AGAIN?` 
   //const playAgain = Document.getElementById('revealButton').innerHtml
    //if  (document.getElementById('revealButton').innerHtml === `PLAY AGAIN?`) {
@@ -366,8 +390,7 @@ playAgain();
 //}
   // pageReload();
  //}
- //console.log(pageReload); this just prints the text-content of the function above in the console. Why?
- //pageReload(); //Om jag har den här så laddar sian om hela tiden från start. Har jag skrivit min funktion fel eller vad?
+ //pageReload(); //If I put this here, the page reloads directly from start over and over.
 
 
 
