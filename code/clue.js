e8518655f0faa50ed24a737b31147f4f49d20bb1
Nadia <63302901+Bluepoisson@users.noreply.github.com>
2020-09-20 12:32:02 +0200

    Images and styling
    Updated image file and started styling the page.

--- code/clue.js
+++ code/clue.js
@@ -6,7 +6,7 @@ const mrGreen = {
   color: "green",
   description: "He has a lot of connections",
   age: 45,
-  image: "assets/green.png",
+  image: "images/green.jpg",
   occupation: "Entrepreneur",
 };
 
@@ -16,7 +16,7 @@ const professorPlum = {
   color: "plum",
   description: "He has a tendency to over explain things",
   age: 45,
-  image: "assets/plum.png",
+  image: "images/plum.jpg",
   occupation: "Professor",
 };
 
@@ -26,7 +26,7 @@ const missScarlet = {
   color: "red",
   description: "Used to getting her ways",
   age: 49,
-  image: "assets/scarlet.png",
+  image: "images/scarlett",
   occupation: "Art dealer",
 };
 
@@ -36,7 +36,7 @@ const mrsPeacock = {
   color: "blue",
   description: "A nuanced yet bold woman",
   age: 55,
-  image: "assets/peacock.png",
+  image: "images/peacock.jpg",
   occupation: "CEO of an oil company",
 };
 
@@ -46,7 +46,7 @@ const colonelMustard = {
   color: "yellow",
   description: "Stubborn know-it-all gentleman",
   age: 65,
-  image: "assets/mustard.png",
+  image: "images/mustard.jpg",
   occupation: "Retired colonel",
 };
 
@@ -56,8 +56,8 @@ const mrsWhite = {
   color: "white",
   description: "Loyal, observant and to the point",
   age: 65,
-  image: "assets/white.png",
-  occupation: "Butler",
+  image: "images/white.jpg",
+  occupation: "Head Maid",
 };
 
 // CREATE OBJECTS FOR ALL THE WEAPONS, ADD MORE CHARACTERISTICS TO THE WEAPONS IF YOU LIKE.
@@ -65,46 +65,64 @@ const mrsWhite = {
 const rope = {
   name: "rope",
   weight: 10,
+  material: "jute",
+  image: "images/weapon/rope-coil.svg",
 };
 
 const knife = {
   name: "knife",
   weight: 5,
+  material: "steel and wood",
+  image: "images/weapon/sacrificial-dagger.svg",
 };
 
 const candlestick = {
   name: "candlestick",
   weight: 25,
+  material: "brass",
+  image: "images/weapon/unlit-candelabra.svg",
 };
 
 const dumbbell = {
   name: "dumbbell",
   weight: 50,
+  material: "steel",
+  image: "",
 };
 
 const poison = {
   name: "poison",
   weight: 2,
+  material: "liquid",
+  image: "images/weapon/poison-bottle.svg",
 };
 
 const axe = {
   name: "axe",
   weight: 15,
+  material: "steel and wood",
+  image: "images/weapon/wood-axe.svg",
 };
 
 const bat = {
   name: "bat",
   weight: 6,
+  material: "aluminium",
+  image: "images/weapon/lead-pipe.svg"
 };
 
 const trophy = {
   name: "trophy",
   weight: 30,
+  material: "brass",
+  image: "",
 };
 
 const pistol = {
   name: "pistol",
   weight: 10,
+  material: "aluminium",
+  image: "images/weapon/revolver.svg"
 };
 
 // THE ROOMS ONLY HAS A NAME SO NO NEED FOR OBJECTS THERE.
@@ -136,7 +154,6 @@ const suspects = [
   mrsPeacock,
   colonelMustard,
   mrsWhite,
-  // ...  and the rest
 ];
 
 const weapons = [
@@ -173,10 +190,11 @@ const pickKiller = () => {
 
   // This will change the background color of the card to the one connected to the chosen killer and show the full name of the killer. Feel free to add more things to show about the killer.
   document.getElementById("killerCard").style.background = mystery.killer.color;
-  document.getElementById(
-    "killerName"
-  ).innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
-  document.getElementById("killerage").innerHTML = `${mystery.killer.age}`;
+  document.getElementById("killerName").innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`;
+  document.getElementById("killerAge").innerHTML = `${mystery.killer.age}`;
+  document.getElementById("killerImage").src = `${mystery.killer.image}`;
+  document.getElementById("killerOccupation").innerHTML = `${mystery.killer.occupation}`;
+  document.getElementById("killerDescription").innerHTML = `${mystery.killer.description}`;
   document.getElementById("killerCard").onclick = pickKiller;
 };
 
@@ -184,9 +202,8 @@ const pickKiller = () => {
 const pickWeapon = () => {
   mystery.weapon = randomSelector(weapons);
 
-  document.getElementById(
-    "weaponName"
-  ).innerHTML = `${mystery.weapon.name} ${mystery.weapon.weight}`;
+  document.getElementById("weaponName").innerHTML = `${mystery.weapon.name} ${mystery.weapon.material}`;
+  document.getElementById("weaponImage").src = `${mystery.weapon.image}`;
   document.getElementById("weaponCard").onclick = pickWeapon;
 };
 
