e2185f62e7d0eab1d3d1459f0cd32042c7f4d324
annahasselstrom <64977313+annahasselstrom@users.noreply.github.com>
2020-09-24 09:58:03 +0200

    Added changes as suggested in code review.

--- code/clue.js
+++ code/clue.js
@@ -5,7 +5,7 @@ const mrGreen = {
   lastName: 'Green',
   color: 'green',
   description: 'He is connected.',
-  age: 'Age: ' + 45,
+  age: 'Age: ' + '45',
   image: 'assets/green.png',
   occupation: 'Occupation: entrepreneur',
   favouriteWeapon: 'knife'
@@ -16,7 +16,7 @@ const colonelMustard = {
   lastName: 'Mustard',
   color: 'yellow',
   description: 'He is an old-school army man.',
-  age: 'Age: ' + 62,
+  age: 'Age: ' + '62',
   image: 'assets/mustard.png',
   occupation: 'Occupation: officer',
   favouriteWeapon: 'dumbbell'
@@ -27,7 +27,7 @@ const mrsPeacock = {
   lastName: 'Peacock',
   color: 'blue',
   description: 'She has many secrets.',
-  age: 'Age: ' + 43,
+  age: 'Age: ' + '43',
   image: 'assets/peacock.png',
   occupation: 'Occupation: artist',
   favouriteWeapon: 'candlestick'
@@ -38,7 +38,7 @@ const mrPlum = {
   lastName: 'Plum',
   color: 'purple',
   description: 'He loves his bees.',
-  age: 'Age: ' + 29,
+  age: 'Age: ' + '29',
   image: 'assets/plum.png',
   occupation: 'Occupation: biologist',
   favouriteWeapon: 'poison'
@@ -49,7 +49,7 @@ const msScarlet = {
   lastName: 'Scarlet',
   color: 'pink',
   description: 'She is super-smart.',
-  age: 'Age: ' + 26,
+  age: 'Age: ' + '26',
   image: 'assets/scarlet.png',
   occupation: 'Occupation: journalist',
   favouriteWeapon: 'bat'
@@ -60,7 +60,7 @@ const mrsWhite = {
   lastName: 'White',
   color: 'white',
   description: 'She is no old biddy.',
-  age: 'Age: ' + 62,
+  age: 'Age: ' + '62',
   image: 'assets/white.png',
   occupation: 'Occupation: housekeeper',
   favouriteWeapon: 'axe'
@@ -70,52 +70,52 @@ const mrsWhite = {
 
 const rope = {
   name: 'rope',
-  weight: 10
+  weight: '10'
 }
 
 const knife = {
   name: 'knife',
   id: 'knife',
-  weight: 5
+  weight: '5'
 }
 
 const candlestick = {
   name: 'candlestick',
-  weight: 25
+  weight: '25'
 }
 
 const dumbbell = {
   name: 'dumbbell',
   id: 'dumbbell',
-  weight: 30
+  weight: '30'
 }
 
 const poison = {
   name: 'poison',
   id: 'poison',
-  weight: 5
+  weight: '5'
 }
 
 const axe = {
   name: 'axe',
   id: 'axe',
-  weight: 25
+  weight: '25'
 }
 
 const bat = {
   name: 'bat',
   id: 'bat',
-  weight: 20
+  weight: '20'
 }
 
 const trophy = {
   name: 'trophy',
-  weight: 30
+  weight: '30'
 }
 
 const pistol = {
   name: 'pistol',
-  weight: 25
+  weight: '25'
 }
 
 
@@ -179,27 +179,26 @@ const mystery = {
 
 const pickKiller = () => {
   mystery.killer = randomSelector(suspects);
-  document.getElementById('loader1').style.display = 'block';
-
+  document.getElementById('loaderKiller').style.display = 'block';
   setTimeout(afterLoaderPickKiller, 1600);
 };
   
 document.getElementById('killerImage').style.filter = 'sepia(100%)';
-document.getElementById('loader1').style.display = 'none'; // prevents loader to begin when page is loaded
+document.getElementById('loaderKiller').style.display = 'none'; // prevents loader to begin when page is loaded
 document.getElementById('killerCard').addEventListener('click', pickKiller); // allows us to connect en event when clicked to the killer card
 
 // FUNCTION - this function runs when loader is done.
 
 const afterLoaderPickKiller = () => {
   document.getElementById('killerImage').style.filter = 'sepia(0%)';
-  document.getElementById('loader1').style.display = 'none';
+  document.getElementById('loaderKiller').style.display = 'none';
   document.getElementById('killerCard').removeEventListener('click', pickKiller);
 
 // FUNCTION - this function randomly changes each killer's favourite weapon.
 
   const shuffleFavouriteWeapon = () => {
-    mystery.favouriteWeapon = randomSelector(weapons)
-  }
+    mystery.favouriteWeapon = randomSelector(weapons);
+  };
  
 // This calls/executes the above function.
 
@@ -207,53 +206,51 @@ const afterLoaderPickKiller = () => {
 
 // These connects the js and the html, and outputs in browser.
 
-  document.getElementById('favouriteWeapon').innerHTML = `Favourite weapon: ${mystery.favouriteWeapon.name}`
-  document.getElementById('killerCard').style.background = mystery.killer.color
-  document.getElementById('killerName').innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}` 
-  document.getElementById('killerImage').src = mystery.killer.image
-  document.getElementById('killerAge').innerHTML = `${mystery.killer.age}` 
-  document.getElementById('killerOccupation').innerHTML = `${mystery.killer.occupation}` 
-  document.getElementById('killerDescription').innerHTML = `${mystery.killer.description}` 
+  document.getElementById('favouriteWeapon').innerHTML = `Favourite weapon: ${mystery.favouriteWeapon.name}`;
+  document.getElementById('killerCard').style.background = mystery.killer.color;
+  document.getElementById('killerName').innerHTML = `${mystery.killer.firstName} ${mystery.killer.lastName}`; 
+  document.getElementById('killerImage').src = mystery.killer.image;
+  document.getElementById('killerAge').innerHTML = mystery.killer.age; 
+  document.getElementById('killerOccupation').innerHTML = mystery.killer.occupation; 
+  document.getElementById('killerDescription').innerHTML = mystery.killer.description; 
 };
 
 // FUNCTION - this function will runs when you click on the weapon card.
 
 const pickWeapon = () => {
-  mystery.weapon = randomSelector(weapons)
-  document.getElementById('loader2').style.display = 'block';
-
+  mystery.weapon = randomSelector(weapons);
+  document.getElementById('loaderWeapon').style.display = 'block';
   setTimeout(afterLoaderPickWeapon, 1600);
 };
 
-document.getElementById('loader2').style.display = 'none';
+document.getElementById('loaderWeapon').style.display = 'none';
 document.getElementById('weaponCard').addEventListener('click', pickWeapon);
 
 // FUNCTION - this function runs when loader is done.
 
 const afterLoaderPickWeapon = () => {
-  document.getElementById('loader2').style.display = 'none';
+  document.getElementById('loaderWeapon').style.display = 'none';
   document.getElementById('weaponCard').removeEventListener('click', pickWeapon);
-  document.getElementById('weaponName').innerHTML = `${mystery.weapon.name}`
+  document.getElementById('weaponName').innerHTML = `${mystery.weapon.name}`;
 };
 
 // FUNCTION - this function runs when you click on the room card.
 
 const pickRoom = () => {
-  mystery.room = randomSelector(rooms)
-  document.getElementById('loader3').style.display = 'block';
-
+  mystery.room = randomSelector(rooms);
+  document.getElementById('loaderRoom').style.display = 'block';
   setTimeout(afterLoaderPickRoom, 1600);
 };
 
-document.getElementById('loader3').style.display = 'none';
+document.getElementById('loaderRoom').style.display = 'none';
 document.getElementById('roomCard').addEventListener('click', pickRoom);
 
 // FUNCTION - This function runs when loader is done.
 
 const afterLoaderPickRoom = () => {
-  document.getElementById('loader3').style.display = 'none';
+  document.getElementById('loaderRoom').style.display = 'none';
   document.getElementById('roomCard').removeEventListener('click', pickRoom);
-  document.getElementById('roomName').innerHTML = `${mystery.room}` 
+  document.getElementById('roomName').innerHTML = `${mystery.room}`;
 };
 
 // Creates a function - revealMystery - that runs when you click reveal button.
